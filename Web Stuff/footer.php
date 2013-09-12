2462c87072bd2be469592ae5737897ec4ad30816
Tushka <69giaptushka@gmail.com>
2013-09-11 22:28:49 -0400

    replacing boswar with rofwar, etc.

--- Web Stuff/footer.php
+++ Web Stuff/footer.php
@@ -2,7 +2,7 @@
         
         <div id="bottom">
             <div id="credits">
-                <p>Powered by IL2 STURMOVIK - Battle of Stalingrad</p>
+                <p>Powered by 777 Studios - Rise of Flight</p>
                 <p>brought to you by =69.GIAP=</p>
             </div>
         </div>
