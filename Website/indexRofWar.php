ce6905d1f140a23be5d0a2bb191aacc8a1fd44f7
69giapmyata <rowbajt@gmail.com>
2013-09-12 19:51:54 +0200

    individual index pages for the two applications

--- Website/indexRofWar.php
+++ Website/indexRofWar.php
@@ -21,7 +21,7 @@
         <div id="container">
     
             <div id="content">
-                <h1>Content BosWar</h1>
+                <h1>Content RofWar</h1>
                 <p>Lorem ipum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
                 </p>
                 
