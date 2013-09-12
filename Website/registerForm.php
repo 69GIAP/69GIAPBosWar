b8830c404590fa22186ff8a15e68810687c7b7dc
69giapmyata <rowbajt@gmail.com>
2013-09-12 16:44:47 +0200

    register form has new layout
    code markup cleaned

--- Website/registerForm.php
+++ Website/registerForm.php
@@ -7,53 +7,60 @@
 
 # Incorporate the MySQL connection script.
 	require ( '../connect_db.php' );
+	
+# Include the webside header
+	include ( 'includes/header.php' );
+	
+# Include the navigation on top
+	include ( 'includes/navigation.php' );
 
-# header information
-	include 'includes/header.php';
 ?>
-        
-				<ul id="navigation">
-                  <li><a href="#" class="menu1"><span></span></a></li>
-                  <li><a href="#" class="menu2"><span></span></a></li>
-                </ul>
-                <ul id="register">
-                  <li><a href="registerForm.php" class="register"><span></span></a></li>
-                  
-					<!-- if the user is not looged on he sees a login button -->
-					<?php include 'includes/loginButton.php'; ?> 
 
-                <li><a href="logout.php" class="logout"><span></span></a></li>   
-                </ul>
+	<div id="wrapper">
+
+        <div id="container">
+    
+            <div id="content">
+            
+                <p><b>Register:</b></p>
+                 
+                <fieldset class="boswar">      
+                    <form name="register" action="register.php" method="post">
+                        <ul>
+                            <li> <label for="username">User Name:</label>
+                            <input type="text" name="username" id="username" size="30" />
+                            </li>
+                            <li> <label for="email">Email Address:</label>
+                            <input type="text" name="email" id="email" size="30" />
+                            </li>
+                            <li> <label for="password">Password:</label>
+                            <input type="password" name="password" id="password" size="30" />
+                            </li>
+                            <li> <label for="password2">Repeat Password:</label>
+                            <input type="password" name="password2" id="password2" size="30" />
+                            </li>
+                            <li> <label for="phone">Telephone Number:</label>
+                            <input type="text" name="phone" id="phone" size="30" />
+                            </li>                        
+                            <li><label for="submit"></label>
+                            <button type="submit" id="submit">Submit</button></li>
+                        <ul>
+                    </form>
+                </fieldset>
+                
             </div>
-        
-        <div id="left"></div>
-        
-        <div id="content">
-            <p><b>Register:</b></p> 
-            <fieldset>      
-                <form name="register" action="register.php" method="post">
-                    <ul>
-                        <li> <label for="username">User Name</label>
-                        <input type="text" name="username" id="username" size="30" />
-                        </li>
-                        <li> <label for="email">Email Address</label>
-                        <input type="text" name="email" id="email" size="30" />
-                        </li>
-                        <li> <label for="password">Password</label>
-                        <input type="password" name="password" id="password" size="30" />
-                        </li>
-                        <li> <label for="password2">Repeat Password</label>
-                        <input type="password" name="password2" id="password2" size="30" />
-                        </li>
-                        <li> <label for="phone">Telephone Number</label>
-                        <input type="text" name="phone" id="phone" size="30" />
-                        </li>                        
-                        <li><label for="submit"></label>
-                        <button type="submit" id="submit">Submit</button></li>
-                    <ul>
-                </form>
-            </fieldset>
-        
+    
         </div>
-        
-<?php include 'includes/footer.php'; ?>
+
+<?php
+	# Include the general sidebar
+	include ( 'includes/sidebar.php' );
+?>	
+
+		<div id="clearing"></div>
+	</div>
+
+<?php
+	# Include the footer
+	include ( 'includes/footer.php' );
+?>
