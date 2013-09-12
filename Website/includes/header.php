17fa5ec9ddf31935eb308fe307cb9dc92bb8d93e
69giapmyata <rowbajt@gmail.com>
2013-09-12 16:39:45 +0200

    new design - holding user logged on information now
    this is visible in the navigation bar - left

--- Website/includes/header.php
+++ Website/includes/header.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 <title>BosWar</title>
+
 <!--  Link external CSS Master file containing all other CSS files -->
-<link href="css/BOSWAR_AllStyles.css" rel="stylesheet" type="text/css" />
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+
 <!-- Include jQuery Library -->
 <script src="js/jquery-1.2.2.pack.js" type="text/javascript"></script>
+
 <!-- Let's do the animation -->
 <script type="text/javascript" src="js/animation.js"></script>
 
-</head>
+<body>
 
-    <body>
+<div id="header">
     
-        <div id="top">
-			<?php 
-			# reference the username next to the navigation bar              
-			include 'includes/logonInfo.php'
-            ?>
+	<?php 
+		# reference the username next to the navigation bar              
+		include 'includes/loggedOnInfo.php'
+    ?>
             
-            <div id="title"></div>
+	<div id="title"></div>
         
