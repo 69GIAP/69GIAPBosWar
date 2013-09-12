626545d0a6e892f1b2ce30d31809517d09658364
69giapmyata <rowbajt@gmail.com>
2013-09-12 19:51:36 +0200

    changed index.php to server as initial decission point

--- Website/index.php
+++ Website/index.php
@@ -1,45 +1,33 @@
-<?php 
-# creates a new session for tracking the user is logged on 
-	session_start(); 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-# Incorporate the MySQL debug script.
-	require ( 'includes/debug.php' );
+<title>BosWar RofWar</title>
+</head>
+<!--  Link external CSS Master file containing all other CSS files -->
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
 
-# Incorporate the MySQL connection script.
-	require ( '../connect_db.php' );
-	
-# Include the webside header
-	include ( 'includes/header.php' );
-	
-# Include the navigation on top
-	include ( 'includes/navigation.php' );
+<body class="index">
 
-?>
-
-	<div id="wrapper">
-
-        <div id="container">
+    <div id="container"></div>
     
-            <div id="content">
-                <h1>Content</h1>
-                <p>Lorem ipum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-                </p>
-                
-                <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-                </p>
-            </div>
+    <div id="indexRofWar">
+        <a href="indexRofWar.php">
+        <img  src="img/RofWar_Title.png" alt="" width="391" height="57" border="0"></a>
+    </div>
     
+    <div id="indexBosWar">
+        <a href="indexBosWar.php">
+        <img src="img/BosWar_Title.png" alt="" width="293" height="57" border="0" ></a>
+    </div>	
+	<div id="footer">
+
+        <div id="credits">
+            <p>brought to you by =69.GIAP=</p>
         </div>
 
-<?php
-	# Include the general sidebar
-	include ( 'includes/sidebar.php' );
-?>	
-
-		<div id="clearing"></div>
 	</div>
+</body>
 
-<?php
-	# Include the footer
-	include ( 'includes/footer.php' );
-?>
+</html>
