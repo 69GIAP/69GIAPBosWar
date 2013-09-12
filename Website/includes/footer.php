cfb6817a425223e9ae20561ef2ce0a60ee06555e
69giapmyata <rowbajt@gmail.com>
2013-09-12 16:33:07 +0200

    new design
    changed div id

--- Website/includes/footer.php
+++ Website/includes/footer.php
@@ -1,9 +1,13 @@
-            <div id="bottom">
-                <div id="credits">
-                    <p>Powered by IL2 STURMOVIK - Battle of Stalingrad</p>
-                    <p>brought to you by =69.GIAP=</p>
-                </div>
-            </div>
-    </body>
+	<div id="footer">
+
+        <div id="credits">
+            <p>Powered by IL2 STURMOVIK - Battle of Stalingrad</p>
+            <p>brought to you by =69.GIAP=</p>
+        </div>
+
+	</div>
+
+	</body>
 
 </html>
+
