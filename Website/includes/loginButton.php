9039193948c103a7699766ec2a4c43767c190ed4
69giapmyata <rowbajt@gmail.com>
2013-09-12 16:40:41 +0200

    new code for the login button decission
    now the code can be included to create the right link dependent on the
    session status.

--- Website/includes/loginButton.php
+++ Website/includes/loginButton.php
@@ -1,21 +1,15 @@
-<?php                 
+<?php 
+
+# show the login button if the user is not logged on         
+       
 	if(!isset($_SESSION["username"])) 
 		{
-		# close html document properly creating the login button  and then exit
-		echo "<li><a href=\"loginForm.php\" class=\"login\"><span></span></a></li>\n";
-		echo "</ul>\n";
-		echo "</div>\n";
-		echo "</div>\n";					
-		
-		echo"<div id=\"left\"></div>\n";
-		# open container 
-		echo "<div id=\"content\">\n";			
-		echo "<p>To view the content of this area please register and log in first.</p>\n";
-
-		echo "</div>\n";
-		
-		include 'includes/footer.php';
-		
-		exit; 
-	}         
-?> 
+		# show the login button
+		echo "		<li><a href=\"loginForm.php\" class=\"login\"><span></span></a></li>\n";
+		}
+	else
+		{
+		# show the logout button
+		echo "    <li><a href=\"logout.php\" class=\"logout\"><span></span></a></li>\n";
+		}
+?>
