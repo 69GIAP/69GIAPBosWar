b656e74ed12e77133e715224b7b349759ca2d6e7
69giapmyata <rowbajt@gmail.com>
2013-09-14 09:58:34 +0200

    footer message is now dynamic as well

--- Website/includes/footer.php
+++ Website/includes/footer.php
@@ -1,9 +1,22 @@
 	<div id="footer">
-
-        <div id="credits">
-            <p>An unofficial addon for IL2 STURMOVIK - Battle of Stalingrad</p>
-            <p>brought to you by =69.GIAP=</p>
-        </div>
+    
+		<?php
+		#dynamic footer information
+		if ($game == "RoF")
+			{
+				echo "<div id=\"credits\">\n";
+				echo "<p>An unofficial addon for Rise of Flight</p>\n";
+				echo "<p>brought to you by =69.GIAP=</p>\n";
+				echo "</div>\n";
+			}
+		if ($game == "BoS")
+			{
+				echo "<div id=\"credits\">\n";
+				echo "<p>An unofficial addon for IL2 STURMOVIK - Battle of Stalingrad</p>\n";
+				echo "<p>brought to you by =69.GIAP=</p>\n";
+				echo "</div>\n";
+			}
+        ?>
 
 	</div>
 
