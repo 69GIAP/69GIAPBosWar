05f340e5d500328bf0a4e6559895feabea430fd5
69giapmyata <rowbajt@gmail.com>
2013-09-16 19:52:38 +0200

    markup change

--- Website/test_change_db.php
+++ Website/test_change_db.php
@@ -9,29 +9,32 @@
 	require ( '../connect_db.php' );
 
 # return name of current default database
-if ($result = mysqli_query($dbc, "SELECT DATABASE()")) {
-    $row = mysqli_fetch_row($result);
-    printf("Default database is %s.<br>\n", $row[0]);
-    mysqli_free_result($result);
-}
+if ($result = mysqli_query($dbc, "SELECT DATABASE()")) 
+	{
+		$row 	= mysqli_fetch_row($result);
+		printf("Default database is %s.<br>\n", $row[0]);
+		mysqli_free_result($result);
+	}
 
 # change db to flanders_eagles db 
 print("Attempting to change to flanders_eagles database.<br>\n");
 $change = mysqli_select_db($dbc, "flanders_eagles");
 print("change = ".$change."<br>\n");
-if ( !$change ) {
-    print("Can not select flanders_eagles database.<br>\n");
-    print("Check that db user has proper permissions for it.<br>\n");
-}
+if ( !$change ) 
+	{
+		print("Can not select flanders_eagles database.<br>\n");
+		print("Check that db user has proper permissions for it.<br>\n");
+	}
 
 # return name of current default database 
-if ($result = mysqli_query($dbc, "SELECT DATABASE()")) {
-    $row = mysqli_fetch_row($result);
-    printf("Now default database is %s.<br>\n", $row[0]);
-    mysqli_free_result($result);
-}
+if ($result = mysqli_query($dbc, "SELECT DATABASE()")) 
+	{
+		$row 	= mysqli_fetch_row($result);
+		printf("Now default database is %s.<br>\n", $row[0]);
+		mysqli_free_result($result);
+	}
 
 # Close connection
-mysqli_close($dbc);
-
+ mysqli_close($dbc);
+ 
 ?>
