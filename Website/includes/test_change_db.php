d038d59b85801940ea6cfc1003b2b6ebee786982
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-09-16 12:48:41 -0400

    moving test_change_db.php to includes

---
+++ Website/includes/test_change_db.php
@@ -0,0 +1,34 @@
+<?php 
+# Incorporate the MySQL debug script.
+	require ( 'debug.php' );
+
+# Incorporate the MySQL connection script.
+	require ( '../connect_db.php' );
+
+# return name of current default database
+if ($result = mysqli_query($dbc, "SELECT DATABASE()")) {
+    $row = mysqli_fetch_row($result);
+    printf("Default database is %s.<br>\n", $row[0]);
+    mysqli_free_result($result);
+}
+
+# change db to flanders_eagles db 
+print("Attempting to change to flanders_eagles database.<br>\n");
+$change = mysqli_select_db($dbc, "flanders_eagles");
+print("change = ".$change."<br>\n");
+if ( !$change ) {
+    print("Can not select flanders_eagles database.<br>\n");
+    print("Check that db user has proper permissions for it.<br>\n");
+}
+
+# return name of current default database 
+if ($result = mysqli_query($dbc, "SELECT DATABASE()")) {
+    $row = mysqli_fetch_row($result);
+    printf("Now default database is %s.<br>\n", $row[0]);
+    mysqli_free_result($result);
+}
+
+# Close connection
+mysqli_close($dbc);
+
+?>
