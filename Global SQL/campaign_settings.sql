dbd741b383afaa69bf11891eee3b2b261abc7ca5
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-09-17 14:15:14 -0400

    update campaign_settings.sql
    new fields for campaign db, host, user and password

--- Global SQL/campaign_settings.sql
+++ Global SQL/campaign_settings.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2013 at 05:48 PM
+-- Generation Time: Sep 17, 2013 at 08:12 PM
 -- Server version: 5.6.11
 -- PHP Version: 5.5.3
 
@@ -26,11 +26,15 @@ SET time_zone = "+00:00";
 -- Table structure for table `campaign_settings`
 --
 
+DROP TABLE IF EXISTS `campaign_settings`;
 CREATE TABLE IF NOT EXISTS `campaign_settings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `simulation` varchar(6) NOT NULL,
   `campaign` varchar(30) NOT NULL,
-  `db_name` varchar(30) NOT NULL,
+  `camp_db` varchar(30) NOT NULL,
+  `camp_host` varchar(30) NOT NULL,
+  `camp_user` varchar(30) NOT NULL,
+  `camp_passwd` varchar(30) NOT NULL,
   `map` varchar(30) NOT NULL,
   `map_locations` varchar(40) NOT NULL,
   `status` int(1) NOT NULL DEFAULT '4',
@@ -47,14 +51,14 @@ CREATE TABLE IF NOT EXISTS `campaign_settings` (
 -- Dumping data for table `campaign_settings`
 --
 
-INSERT INTO `campaign_settings` (`id`, `simulation`, `campaign`, `db_name`, `map`, `map_locations`, `status`, `show_airfield`, `finish_flight_only_landed`, `redAirAdmin`, `redGroundAdmin`, `blueAirAdmin`, `blueGroundAdmin`) VALUES
-(1, 'RoF', 'Bloody April', 'bloody_april', 'Western Front', 'rof_westernfront_locations', 2, 1, 1, 0, 0, 0, 0),
-(2, 'RoF', 'Flanders Eagles', 'flanders_eagles', 'Channel', 'rof_channel_locations', 3, 1, 1, 0, 0, 0, 0),
-(3, 'RoF', 'Lake', 'lake', 'Lake', 'rof_lake_locations', 1, 0, 1, 0, 0, 0, 0),
-(4, 'RoF', 'Skies of the Empires II', 'skies_of_the_empires_ii', 'Verdun', 'rof_verdun_locations', 2, 0, 1, 0, 0, 0, 0),
-(5, 'BoS', 'Stalingrad', 'stalingrad', 'Stalingrad', 'bos_stalingrad_locations', 4, 0, 1, 0, 0, 0, 0),
-(6, 'RoF', 'Yankee Doodle', 'yankee_doodle', 'Verdun', 'rof_verdun_locations', 2, 1, 1, 0, 0, 0, 0),
-(7, 'RoF', 'Skies of the Empires', 'skies_of_the_empires', 'Verdun', 'rof_verdun_locations', 2, 0, 1, NULL, NULL, NULL, NULL);
+INSERT INTO `campaign_settings` (`id`, `simulation`, `campaign`, `camp_db`, `camp_host`, `camp_user`, `camp_passwd`, `map`, `map_locations`, `status`, `show_airfield`, `finish_flight_only_landed`, `redAirAdmin`, `redGroundAdmin`, `blueAirAdmin`, `blueGroundAdmin`) VALUES
+(1, 'RoF', 'Bloody April', 'bloody_april', '', '', '', 'Western Front', 'rof_westernfront_locations', 2, 1, 1, 0, 0, 0, 0),
+(2, 'RoF', 'Flanders Eagles', 'flanders_eagles', 'localhost', 'rofwar', 'rofwar', 'Channel', 'rof_channel_locations', 3, 1, 1, 0, 0, 0, 0),
+(3, 'RoF', 'Lake', 'lake', '', '', '', 'Lake', 'rof_lake_locations', 1, 0, 1, 0, 0, 0, 0),
+(4, 'RoF', 'Skies of the Empires II', 'skies_of_the_empires_ii', '', '', '', 'Verdun', 'rof_verdun_locations', 2, 0, 1, 0, 0, 0, 0),
+(5, 'BoS', 'Stalingrad', 'stalingrad', '', '', '', 'Stalingrad', 'bos_stalingrad_locations', 4, 0, 1, 0, 0, 0, 0),
+(6, 'RoF', 'Yankee Doodle', 'yankee_doodle', '', '', '', 'Verdun', 'rof_verdun_locations', 2, 1, 1, 0, 0, 0, 0),
+(7, 'RoF', 'Skies of the Empires', 'skies_of_the_empires', '', '', '', 'Verdun', 'rof_verdun_locations', 2, 0, 1, NULL, NULL, NULL, NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
