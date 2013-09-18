c31c28a1e0caf43b5428fcfcac9c80a1caa18549
=69.GIAP=MYATA <rowbajt@gmail.com>
2013-09-18 13:15:58 +0200

    new buttons added, registry btn disapears if user is already logged on

--- Website/includes/navigation.php
+++ Website/includes/navigation.php
@@ -1,9 +1,13 @@
+
+<!-- The variable I hand over with the link is stored to the SESSION["btn"] variable and defines the look of the sidebar -->
 <ul id="navigation">
-    <li><a href="#" class="menu1"><span></span></a></li>
-    <li><a href="#" class="menu2"><span></span></a></li>
+    <li><a href="LoggedOn_Admin.php" class="home"><span></span></a></li>
+    <li><a href="UserManagement.php?btn=usermgmt" class="userMgmt"><span></span></a></li>
+	<li><a href="CampaignManagment_Admin.php?btn=campmgmt" class="campMgmt"><span></span></a></li>
 </ul>
 <ul id="register">
-    <li><a href="registerForm.php" class="register"><span></span></a></li>       
-        <!-- determine if the user sees a login or logout button depending on an active session -->
-        <?php include 'includes/loginButton.php'; ?>
+    <!-- determine if the user sees a register button depending on SESSION username -->
+    <?php include 'includes/registerButton.php'; ?>       
+    <!-- determine if the user sees a login or logout button depending on an active session -->
+    <?php include 'includes/loginButton.php'; ?>
 </ul>
