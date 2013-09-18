cb15536c93309a7e3af2e44b8e7755419b6983dc
=69.GIAP=MYATA <rowbajt@gmail.com>
2013-09-18 16:20:10 +0200

    markup

--- Website/includes/connect2campaign.php
+++ Website/includes/connect2campaign.php
@@ -4,18 +4,18 @@
 # =69.GIAP=TUSHKA
 # Sept 16,2013
 #
-function connect_campaign ($host,$user,$password,$db) {
-
-# debugging
-print "<p>debugging connect2campaign.php</p>\n";
-print "<p>host = $host, user = $user, password = $password, db = $db</p><br>\n";
-
-# make connection to campaign with input variables
-$camp_link = mysqli_connect ( "$host", "$user" , "$password" , "$db" )
-OR die ('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
-
-mysqli_set_charset( $camp_link , 'utf8' ) ;
-
-return $camp_link;
-}
+	function connect_campaign ($host,$user,$password,$db) 
+		{		
+			# debugging
+			print "<p>debugging connect2campaign.php</p>\n";
+			print "<p>host = $host, user = $user, password = $password, db = $db</p><br>\n";
+			
+			# make connection to campaign with input variables
+			$camp_link = mysqli_connect ( "$host", "$user" , "$password" , "$db" )
+			OR die ('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+			
+			mysqli_set_charset( $camp_link , 'utf8' ) ;
+			
+			return $camp_link;
+		}
 ?>
