7e5c40e13546635ef671251b15086e7e0c0f867a
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-09-17 21:49:44 -0400

    update Campaign_Admin2.php
    It now uses data from campaign_settings (in the global db) to connect to
    a specific campaign database selected in the previous page
    (Campaign_Admin.php).  This gives us the flexibility we need.

--- Website/Campaign_Admin2.php
+++ Website/Campaign_Admin2.php
@@ -2,6 +2,9 @@
 # creates a new session for tracking the user is logged on 
 	session_start(); 
 
+# Incorporate the MySQL connection script.
+	require ( '../connect_db.php' );
+	
 # Incorporate the MySQL debug script.
 	require ( 'includes/debug.php' );
 
@@ -14,19 +17,40 @@
 #  include connect2_db.php (defines connect_campaign())
          include ( 'includes/connect2campaign.php' );
 
-
-
 ?>
 
 	<div id="wrapper">
 
         <div id="container">
-    
-            <div id="content">
+	    <div id="content">
                <h2>Campaign Admin2</h2>
 <?php
-	       $link = connect_campaign('localhost',"rofwar","rofwar","flanders_eagles");
-	        include ( "includes/test_change_db.php" ); ?>
+    # get campaign database name from previous POST.
+       $camp_db = $_POST["db"];
+
+    # use it to get remaining variables
+	$query = "SELECT * from campaign_settings where camp_db = '$camp_db'";   
+	if(!$result = $dbc->query($query)) {
+		die('There was an error running the query [' . $dbc->error . ']');
+       	}
+	
+	if ($result = mysqli_query($dbc, $query)) {
+        	/* fetch associative array */
+       		while ($obj = mysqli_fetch_object($result)) {
+			$camp_host	=($obj->camp_host);
+			$camp_user	=($obj->camp_user);
+			$camp_passwd	=($obj->camp_passwd);
+		}
+	} 
+
+	# use this information to connect to campaign 
+	$camp_link = connect_campaign("$camp_host","$camp_user","$camp_passwd","$camp_db");
+
+	# do whatever is needed from the campaign database
+
+	# Close the camp_link connection
+	mysqli_close($camp_link);
+?>
             </div>
     
         </div>
@@ -40,6 +64,9 @@
 	</div>
 
 <?php
+	# Close the dbc connection
+	mysqli_close($dbc);
+
 	# Include the footer
 	include ( "includes/footer.php" );
 ?>
