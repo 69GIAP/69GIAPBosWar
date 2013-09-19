980582998812853d703159907fba7e054855fde5
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-09-18 21:36:54 -0400

    update Campaign_Admin2.php
    linking to rof_parse_log.php and removing unnecessary tab characters.

--- Campaign_Admin2.php
+++ Campaign_Admin2.php
@@ -1,21 +1,21 @@
 <?php 
 # creates a new session for tracking the user is logged on 
-	session_start(); 
+session_start(); 
 
 # Incorporate the MySQL connection script.
-	require ( '../connect_db.php' );
+require ( '../connect_db.php' );
 	
 # Incorporate the MySQL debug script.
-	require ( 'includes/debug.php' );
+require ( 'includes/debug.php' );
 
 # Include the webside header
-	include ( 'includes/header.php' );
+include ( 'includes/header.php' );
 	
 # Include the navigation on top
-	include ( 'includes/navigation.php' );
+include ( 'includes/navigation.php' );
 
 #  include connect2CampaignFunction.php (defines connect2campaign())
-	include ( 'includes/connect2CampaignFunction.php' );
+include ( 'includes/connect2CampaignFunction.php' );
 
 ?>
 
@@ -25,42 +25,48 @@
 	    <div id="content">
         	
                <h2>Campaign Admin2</h2>
-				<?php
-                    # This redirects the user to the Login screen if he gets here and is not logged on
-                    include ( 'includes/errorNotLoggedOn.php' );
+<?php
+# This redirects the user to the Login screen if he gets here and is not logged on
+include ( 'includes/errorNotLoggedOn.php' );
 					
-                    # get campaign database name from previous POST.
-                       $_SESSION['camp_db'] = $_POST["db"];
-					   $camp_db = $_SESSION['camp_db'];
-					   echo "<p>The chosen campaign is stored to the SESSION variable \$_SESSION['camp_db'] and can be reused from now on to work with it.</p>\n";
-					   echo "<p>Currectly the variable is filled with <b>$camp_db</b>.</p>\n";
+# get campaign database name from previous POST.
+$_SESSION['camp_db'] = $_POST["db"];
+$camp_db = $_SESSION['camp_db'];
+
+echo "<p>The chosen campaign is stored to the SESSION variable \$_SESSION['camp_db'] and can be reused from now on to work with it.</p>\n";
+
+echo "<p>Currectly the variable is filled with <b>$camp_db</b>.</p>\n";
                 
-                    # use it to get remaining variables
-                    $query = "SELECT * from campaign_settings where camp_db = '$camp_db'";   
-                    if(!$result = $dbc->query($query)) 
-						{
-                        	die('There was an error running the query [' . $dbc->error . ']');
-                        }
+# use it to get remaining variables
+$query = "SELECT * from campaign_settings where camp_db = '$camp_db'";   
+if(!$result = $dbc->query($query)) {
+	die('There was an error running the query [' . $dbc->error . ']');
+}
                     
-                    if ($result = mysqli_query($dbc, $query)) 
-						{
-                            /* fetch associative array */
-                            while ($obj = mysqli_fetch_object($result)) 
-								{
-									$camp_host	=($obj->camp_host);
-									$camp_user	=($obj->camp_user);
-									$camp_passwd	=($obj->camp_passwd);
-								}
-						} 
+if ($result = mysqli_query($dbc, $query)) {
+	/* fetch associative array */
+	while ($obj = mysqli_fetch_object($result)) {
+		$campaign	=($obj->campaign);
+		$camp_host	=($obj->camp_host);
+		$camp_user	=($obj->camp_user);
+		$camp_passwd	=($obj->camp_passwd);
+	}
+} 
                 
-                    # use this information to connect to campaign 
-                    $camp_link = connect2campaign("$camp_host","$camp_user","$camp_passwd","$camp_db");
+# use this information to connect to campaign 
+$camp_link = connect2campaign("$camp_host","$camp_user","$camp_passwd","$camp_db");
                 
-                    # do whatever is needed from the campaign database
+# do whatever is needed from the campaign database
+print "<h2>Processing statistics for $campaign</h2><br>\n";
+print "We won't actually do this in the release, but this provides a sandbox for integrating the parser with the campaign database.<br>\n";
+
+# include rof_parse_log.php for development purposes
+include ( 'rof_parse_log.php' );
+
                     
-                    # Close the camp_link connection
-                    mysqli_close($camp_link);
-                ?>
+# Close the camp_link connection
+mysqli_close($camp_link);
+?>
             </div>
     
         </div>
