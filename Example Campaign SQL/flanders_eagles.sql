e146358faaecb323e2c7f27035f4d20ec5690a02
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-09-19 13:14:18 -0400

    update flanders_eagles.sql
    adding rof_countries table to support parser

--- Example Campaign SQL/flanders_eagles.sql
+++ Example Campaign SQL/flanders_eagles.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 19, 2013 at 05:09 AM
+-- Generation Time: Sep 19, 2013 at 07:12 PM
 -- Server version: 5.6.11
 -- PHP Version: 5.5.3
 
@@ -668,6 +668,39 @@ INSERT INTO `rof_coalitions` (`CoalID`, `Coalitionname`) VALUES
 (5, 'Knights'),
 (6, 'Corsairs'),
 (7, 'Future');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rof_countries`
+--
+
+DROP TABLE IF EXISTS `rof_countries`;
+CREATE TABLE IF NOT EXISTS `rof_countries` (
+  `id` int(11) NOT NULL,
+  `ckey` int(11) NOT NULL,
+  `countryname` varchar(30) NOT NULL,
+  `countryadj` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `rof_countries`
+--
+
+INSERT INTO `rof_countries` (`id`, `ckey`, `countryname`, `countryadj`) VALUES
+(0, 0, 'Neutral', 'neutral'),
+(0, 101, 'France', 'French'),
+(0, 102, 'Great Britain', 'British'),
+(0, 103, 'USA', 'American'),
+(0, 104, 'Italy', 'Italian'),
+(0, 105, 'Russia', 'Russian'),
+(0, 501, 'Germany', 'German'),
+(0, 502, 'Austro-Hungary', 'Austro-Hungarian'),
+(0, 600, 'Future Country', 'Future'),
+(0, 610, 'War Dogs Country', 'War Dogs'),
+(0, 620, 'Mercenaries Country', 'Mercenaries'),
+(0, 630, 'Knights Country', 'Knights'),
+(0, 640, 'Corsairs Country', 'Corsairs');
 
 -- --------------------------------------------------------
 
