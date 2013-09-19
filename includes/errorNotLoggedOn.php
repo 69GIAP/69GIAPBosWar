22bd31138da3445b48c4b034c7aea3dc69a99efc
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-09-19 00:19:12 -0400

    removing duplicated directories

--- includes/errorNotLoggedOn.php
+++
@@ -1,7 +0,0 @@
-<?php
-    # check if the content is freed to display              
-    if(!isset($_SESSION["username"])) 
-        {	
-			header("Location: loginForm.php");;
-        }
-?>        
