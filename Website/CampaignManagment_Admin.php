b085a65e5e0f3c2fbb6c0d1f9b724923bc41eeca
=69.GIAP=MYATA <rowbajt@gmail.com>
2013-09-22 23:05:17 +0200

    fixed link to parser intermediate section

--- Website/CampaignManagment_Admin.php
+++ Website/CampaignManagment_Admin.php
@@ -21,21 +21,21 @@
                 	# This redirects the user to the Login screen if he tries to press a button and is not logged on
 					include ( 'includes/errorNotLoggedOn.php' );
 					
-					/* Obsolete as already chosen on Home screen
+					# Obsolete as already chosen on Home screen
 					# show campaigns due to User role
 					if ($userRole == "administrator")
 						{
 							include ( 'includes/processCampaignLogs.php' );
 						}
-					if ($userRole == "commander")
+					/*if ($userRole == "commander")
 						{
 							include ( 'includes/getCampaignsCommander.php' );
-						}
+						}*/
 					if ($userRole == "viewer")
 						{
 							echo "<p>You don't have the necessary rights to view this content.</p>\n";
 						}
-					*/
+					
 				?>
             </div>
     
