d9fac637525cbb212b855a558216acf81a5231f4
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-09-24 14:01:09 -0400

    add new table campaign_missions
    I will use this to track missions for stats, but it may also be useful
    for tracking which mission files have been built or mission building
    status.  Both the mission_file and MissionID columns must be unique.

---
+++ Global SQL/campaign_missions.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2013 at 07:55 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `boswar_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign_missions`
+--
+
+DROP TABLE IF EXISTS `campaign_missions`;
+CREATE TABLE IF NOT EXISTS `campaign_missions` (
+  `id` smallint(1) NOT NULL AUTO_INCREMENT,
+  `mission_file` varchar(50) NOT NULL,
+  `MissionID` varchar(50) NOT NULL,
+  `mission_status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `mission_file` (`mission_file`),
+  UNIQUE KEY `MissionID` (`MissionID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
