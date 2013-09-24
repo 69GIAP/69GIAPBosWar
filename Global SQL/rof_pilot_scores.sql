2837fc14fdfc4b3f07d4bf26645679c02df35dc6
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-09-24 13:01:16 -0400

    update gunner and pilot scores tables
    adding new column to each (mgid - for mission's gunner id and mpid for
    mission's pilot id)

--- Global SQL/rof_pilot_scores.sql
+++ Global SQL/rof_pilot_scores.sql
@@ -1,55 +1,42 @@
-CREATE DATABASE  IF NOT EXISTS `boswar_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `boswar_db`;
--- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
 --
--- Host: 10.0.0.57    Database: boswar_db
--- ------------------------------------------------------
--- Server version	5.6.13
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2013 at 06:59 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `boswar_db`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `rof_pilot_scores`
 --
 
 DROP TABLE IF EXISTS `rof_pilot_scores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rof_pilot_scores` (
+CREATE TABLE IF NOT EXISTS `rof_pilot_scores` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `MissionID` varchar(50) NOT NULL,
   `PilotName` varchar(40) NOT NULL,
+  `mpid` int(11) NOT NULL,
   `PilotState` int(11) NOT NULL,
   `PilotScore` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Dumping data for table `rof_pilot_scores`
---
-
-LOCK TABLES `rof_pilot_scores` WRITE;
-/*!40000 ALTER TABLE `rof_pilot_scores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rof_pilot_scores` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-09-24 15:23:40
