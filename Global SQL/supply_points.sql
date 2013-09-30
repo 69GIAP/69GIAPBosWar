94e9d36a8f9cfb5a7b68b0c78f4264240924c339
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-09-30 16:41:59 -0400

    adding supply_points.sql
    Note that x and z positions are integers.  That is good enough for a
    supply point.

---
+++ Global SQL/supply_points.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 30, 2013 at 10:38 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `boswar_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supply_points`
+--
+
+DROP TABLE IF EXISTS `supply_points`;
+CREATE TABLE IF NOT EXISTS `supply_points` (
+  `id` tinyint(1) NOT NULL,
+  `xPos` smallint(1) NOT NULL,
+  `zPos` smallint(6) NOT NULL,
+  `CoalID` smallint(6) NOT NULL,
+  `supplypointName` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `supplypointName` (`supplypointName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
