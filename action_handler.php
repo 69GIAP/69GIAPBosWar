3ecde270b3982e3d134f48da4ccf6c4173c74d43
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-09-29 21:44:14 -0400

    cleaning up duplicated files

--- action_handler.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = $_POST['name'];
-$mail = $_POST['mail'];
-$comment = $_POST['comment'];
-echo "<p>Thanks for this comment $name</p>";
-echo "<p><i> $comment </i></p>";
-echo "<p>We will reply to $mail</p>";
-
-?>
--- test.php
+++
@@ -1,8 +0,0 @@
-# V1.0
-# Stenka 10/9/13
-# Php version of inbox table to load into airfields table runs after inbox load
-<?php
-# require is connecting user peter to stalingrad1 database
-# here
-require('../connect_db.php');
-if(4>2){echo '<br>Four is bigger than 2';}
