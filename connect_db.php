673d67ab97d083ed9b4489887907d427e15b290d
69giapstenka <ptthome@free.fr>
2013-10-03 22:16:42 +0200

    not sure what it wants here

--- connect_db.php
+++ connect_db.php
@@ -3,7 +3,7 @@
 # Connect on '10.0.0.57:3306' for user 'manager' 
 # with password 'manager' to database 'boswar_db'.
 $dbc = mysqli_connect 
- ( '10.0.0.57:3306' , 'manager' , 'manager' , 'boswar_db' )
+ ( 'localhost' , 'peter' , 'grobble5' , 'boswar_db' )
 OR die 
  ( mysqli_connect_error() ) ;
 # Set encoding to match PHP script encoding.
