b17bc3558528a20f275da15a717b470544d63939
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-03 21:14:32 -0400

    Revert "update rof_kills.sql"
    This reverts commit ed113ab936e7d49bb8b17135bb31fbae8e209bf6.

--- Global SQL/rof_kills.sql
+++ Global SQL/rof_kills.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2013 at 12:18 AM
+-- Generation Time: Oct 03, 2013 at 06:14 PM
 -- Server version: 5.6.11
 -- PHP Version: 5.5.3
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `skies_of_the_empires`
+-- Database: `boswar_db`
 --
 
 -- --------------------------------------------------------
@@ -28,31 +28,20 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `rof_kills`;
 CREATE TABLE IF NOT EXISTS `rof_kills` (
-  `id` smallint(1) unsigned NOT NULL AUTO_INCREMENT,
-  `MissionID` varchar(60) NOT NULL,
+  `id` smallint(1) unsigned NOT NULL,
+  `MissionID` varchar(50) NOT NULL,
   `clocktime` time NOT NULL,
   `attackerID` mediumint(1) NOT NULL,
   `attackerName` varchar(50) NOT NULL,
   `attackerCountry` smallint(1) NOT NULL,
   `attackerCoalID` tinyint(1) unsigned NOT NULL,
-  `action` varchar(20) NOT NULL,
   `targetID` mediumint(1) NOT NULL,
   `targetName` varchar(50) NOT NULL,
   `targetCountry` smallint(1) unsigned NOT NULL,
   `targetCoalID` tinyint(1) unsigned NOT NULL,
   `targetValue` smallint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `rof_kills`
---
-
-INSERT INTO `rof_kills` (`id`, `MissionID`, `clocktime`, `attackerID`, `attackerName`, `attackerCountry`, `attackerCoalID`, `action`, `targetID`, `targetName`, `targetCountry`, `targetCoalID`, `targetValue`) VALUES
-(1, 'SKIES OF THE EMPIRES 1919.mission-1919.1.2-8:30:0', '08:59:54', -1, 'Intrinsic', 0, 0, '', 0, '', 0, 0, 0),
-(2, 'SKIES OF THE EMPIRES 1919.mission-1919.1.2-8:30:0', '09:31:13', -1, 'Intrinsic', 0, 0, '', 0, '', 0, 0, 0),
-(3, 'SKIES OF THE EMPIRES 1919.mission-1919.1.2-8:30:0', '10:00:09', -1, 'Intrinsic', 0, 0, '', 0, '', 0, 0, 0),
-(4, 'SKIES OF THE EMPIRES 1919.mission-1919.1.2-8:30:0', '10:00:49', -1, 'Intrinsic', 0, 0, '', 0, '', 0, 0, 0);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
