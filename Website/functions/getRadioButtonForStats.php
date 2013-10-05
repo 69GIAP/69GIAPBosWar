aecd0c0994605f76d36a35b2812f713f769d6092
=69.GIAP=MYATA <rowbajt@gmail.com>
2013-10-05 21:38:26 +0200

    fixed radio button display issue

--- Website/functions/getRadioButtonForStats.php
+++ Website/functions/getRadioButtonForStats.php
@@ -11,37 +11,23 @@ if(!$result = $db_link->query($query))
 	
 if ($result = mysqli_query($db_link, $query))
 	{
-		echo "<table width=\"auto\" align=\"left\" cellpadding=\"2px\" >\n";
-		
-		echo "	<colgroup>\n";
-		echo "		<col width=\"15px\"		\>\n";
-		echo "		<col width=\"180px\"	\>\n";
-		echo "		<col width=\"180px\"	\>\n";
-		echo "		<col width=\"100px\"	\>\n";
-		echo "	</colgroup>\n";	
-					
-		echo "	<tr>\n";
-		echo "		<th align=\"left\"> <font color=\"#A8A8A8\" >			</font></th>\n";
-		echo "		<th align=\"left\"> <font color=\"#A8A8A8\" >Campaign	</font></th>\n";
-		echo "		<th align=\"left\"> <font color=\"#A8A8A8\" >Database	</font></th>\n";		
-		echo "		<th align=\"left\"> <font color=\"#A8A8A8\" >Map		</font></th>\n";
-		echo "	</tr>\n";		
-		 /* fetch associative array */
+		echo "<div class=\"radio\">\n"; 
+		$i = rand(); 
+		/* fetch associative array */
 		 while ($obj = mysqli_fetch_object($result)) 
 		{
 			$campaign	=($obj->campaign);
 			$camp_db	=($obj->camp_db);
 			$map		=($obj->map);
 			$simulation	=($obj->simulation);
-
-			echo "	<tr>\n";
-			echo "		<td><input type=\"radio\" name=\"camp_db\" value=$camp_db></td>\n";
-			echo "		<td><b>".$campaign.	"</b></td>\n";
-			echo "		<td>"	.$camp_db.	"</td>\n";
-			echo "		<td>"	.$map.		"</td>\n";
-			echo "	</tr>\n";
+			echo "<p>\n";
+			echo "	<input id=\"$i\" type=\"radio\" name=\"camp_db\" value=$camp_db>";
+			echo "	<label for=\"$i\"><b>".$campaign."</b> -  ".$camp_db." db - ".$map." map (".$simulation.")</label>  \n";
+			echo "</p>\n";
+			$i ++;
 		}
-		echo "</table><br>\n";
+		echo "</div>\n";
 	}
 }
+
 ?>
