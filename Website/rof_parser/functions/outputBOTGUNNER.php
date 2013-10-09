b878744ecbe60b2cab81528f91f294c09d45b255
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-09 13:56:08 -0400

    update outputBOTGUNNER
    switching from '$BotName' to '$objectdesc' to match SHIPS and TRAINS...
    so can eventually move the descriptions to a new column in
    rof_object_properties (and out of the parser, thus simplifying it.)

--- Website/rof_parser/functions/outputBOTGUNNER.php
+++ Website/rof_parser/functions/outputBOTGUNNER.php
@@ -1,31 +1,31 @@
 <?php
 // BOTGUNNER
 // translate BotGunner into more natural description
-// BOSWAR version 1.2
-// Oct 5, 2013
+// BOSWAR version 1.3
+// Oct 9, 2013
 function BOTGUNNER($type) {
    // translate BotGunner into more natural description
-   global $BotName; // BotGunner description
+   global $objectdesc; // object description
 
    // used in Gotha and DFW
-   if ($type == "BotGunnerG5_1") { $BotName = "gunner"; } 
-   elseif ($type == "BotGunnerG5_2") { $BotName = "gunner"; } 
+   if ($type == "BotGunnerG5_1") { $objectdesc = "gunner"; } 
+   elseif ($type == "BotGunnerG5_2") { $objectdesc = "gunner"; } 
    // used in Gotha and BW12
-   if ($type == "BotGunnerBacker") { $BotName = "Becker gunner"; } 
+   if ($type == "BotGunnerBacker") { $objectdesc = "Becker gunner"; } 
    // used in HP400 and Felixstowe F2A
-   elseif ($type == "BotGunnerHP400_1") { $BotName = "nose gunner"; }
-   elseif ($type == "BotGunnerDavis") { $BotName = "Davis gunner"; } 
+   elseif ($type == "BotGunnerHP400_1") { $objectdesc = "nose gunner"; }
+   elseif ($type == "BotGunnerDavis") { $objectdesc = "Davis gunner"; } 
    // used in Breguet14, Bristol F2B and F.E.2b
-   elseif ($type == "BotGunnerBreguet14") { $BotName = "gunner"; }
+   elseif ($type == "BotGunnerBreguet14") { $objectdesc = "gunner"; }
    // used in RE8 and DH4
-   elseif ($type == "BotGunnerRE8") { $BotName = "gunner"; }
-   elseif ($type == "BotGunnerHP400_2") { $BotName = "Handley Page 0/400 dorsal gunner";}
-   elseif ($type == "BotGunnerHP400_2_WM") { $BotName = "Handley Page 0/400 dorsal gunner";}
-   elseif ($type == "BotGunnerHP400_3") { $BotName = "Handley Page 0/400 ventral gunner";}
-   elseif ($type == "BotGunnerFelix_top-twin") { $BotName = "Felixstowe F2A top gunner"; }
-   elseif ($type == "BotGunnerFe2_sing") { $BotName = "F.E.2b gunner"; }
-   elseif ($type == "BotGunnerBW12") { $BotName = "Brandenburg W12 gunner"; }
-   elseif ($type == "BotGunnerHCL2") { $BotName = "Halberstadt CL.II gunner"; }
+   elseif ($type == "BotGunnerRE8") { $objectdesc = "gunner"; }
+   elseif ($type == "BotGunnerHP400_2") { $objectdesc = "Handley Page 0/400 dorsal gunner";}
+   elseif ($type == "BotGunnerHP400_2_WM") { $objectdesc = "Handley Page 0/400 dorsal gunner";}
+   elseif ($type == "BotGunnerHP400_3") { $objectdesc = "Handley Page 0/400 ventral gunner";}
+   elseif ($type == "BotGunnerFelix_top-twin") { $objectdesc = "Felixstowe F2A top gunner"; }
+   elseif ($type == "BotGunnerFe2_sing") { $objectdesc = "F.E.2b gunner"; }
+   elseif ($type == "BotGunnerBW12") { $objectdesc = "Brandenburg W12 gunner"; }
+   elseif ($type == "BotGunnerHCL2") { $objectdesc = "Halberstadt CL.II gunner"; }
    else { $objecttype = "unexpected Botgunner $type<br>"; }
    
 
