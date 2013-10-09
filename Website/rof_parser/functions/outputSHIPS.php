78b334d53295c89b3ce35b5a9c8702bfa7990b9a
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-09 11:16:39 -0400

    update outputSHIPS.php
    adding three more ships

--- Website/rof_parser/functions/outputSHIPS.php
+++ Website/rof_parser/functions/outputSHIPS.php
@@ -9,11 +9,14 @@ function SHIPS($type) {
    if ($type == "ship_stat_pass") { $targetdesc = "stationary passenger ship"; } 
    elseif ($type == "ship_stat_tank") { $targetdesc = "stationary tanker ship"; } 
    elseif ($type == "ship_stat_cargo") { $targetdesc = "stationary cargo ship"; } 
+   elseif ($type == "Cargo Ship") { $targetdesc = "cargo ship"; }
+   elseif ($type == "Passenger Ship") { $targetdesc = "passenger ship"; }
    elseif ($type == "Tanker Ship") { $targetdesc = "tanker ship"; }
    elseif ($type == "HMS submarine") { $targetdesc = "submarine"; }
    elseif ($type == "GER submarine") { $targetdesc = "submarine"; } 
    elseif ($type == "HMS cruiser") { $targetdesc = "cruiser"; }
    elseif ($type == "GER cruiser") { $targetdesc = "cruiser"; }
+   elseif ($type == "GER light cruiser") { $targetdesc = "light cruiser"; }
    elseif ($type == "frpenicheaaa") { $targetdesc = "peniche AAA barge";}
    elseif ($type == "gerpenicheaaa") { $targetdesc = "peniche AAA barge";}
    else { $targetdesc = "unexpected ship type $type<br>"; }
