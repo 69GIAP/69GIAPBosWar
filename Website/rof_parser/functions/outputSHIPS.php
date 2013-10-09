eba9fd7bd2d850943b71944ba7746100751dc7b9
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-09 13:52:10 -0400

    update outputSHIPS
    Now using same '$objectdesc' as TRAINS ... in anticipation of eventually
    moving these descriptions into rof_object_properties where they could be
    edited at will for special campaigns.

--- Website/rof_parser/functions/outputSHIPS.php
+++ Website/rof_parser/functions/outputSHIPS.php
@@ -1,25 +1,23 @@
 <?php
 // SHIPS
 // translate ship type into more natural description
-// BOSWAR version 1.0
-// Oct 6, 2013
+// BOSWAR version 1.2
+// Oct 9, 2013
 function SHIPS($type) {
-   global $targetdesc; // target description
+   global $objectdesc; // object description
 
-   if ($type == "ship_stat_pass") { $targetdesc = "stationary passenger ship"; } 
-   elseif ($type == "ship_stat_tank") { $targetdesc = "stationary tanker ship"; } 
-   elseif ($type == "ship_stat_cargo") { $targetdesc = "stationary cargo ship"; } 
-   elseif ($type == "Cargo Ship") { $targetdesc = "cargo ship"; }
-   elseif ($type == "Passenger Ship") { $targetdesc = "passenger ship"; }
-   elseif ($type == "Tanker Ship") { $targetdesc = "tanker ship"; }
-   elseif ($type == "HMS submarine") { $targetdesc = "submarine"; }
-   elseif ($type == "GER submarine") { $targetdesc = "submarine"; } 
-   elseif ($type == "HMS cruiser") { $targetdesc = "cruiser"; }
-   elseif ($type == "GER cruiser") { $targetdesc = "cruiser"; }
-   elseif ($type == "GER light cruiser") { $targetdesc = "light cruiser"; }
-   elseif ($type == "frpenicheaaa") { $targetdesc = "peniche AAA barge";}
-   elseif ($type == "gerpenicheaaa") { $targetdesc = "peniche AAA barge";}
-   else { $targetdesc = "unexpected ship type $type<br>"; }
-
+   if ($type == "ship_stat_pass") { $objectdesc = "stationary passenger ship"; } 
+   elseif ($type == "ship_stat_tank") { $objectdesc = "stationary tanker ship"; } 
+   elseif ($type == "ship_stat_cargo") { $objectdesc = "stationary cargo ship"; } 
+   elseif ($type == "Cargo Ship") { $objectdesc = "cargo ship"; }
+   elseif ($type == "Passenger Ship") { $objectdesc = "passenger ship"; }
+   elseif ($type == "Tanker Ship") { $objectdesc = "tanker ship"; }
+   elseif ($type == "HMS submarine") { $objectdesc = "submarine"; }
+   elseif ($type == "GER submarine") { $objectdesc = "submarine"; } 
+   elseif ($type == "HMS light cruiser") { $objectdesc = "light cruiser"; }
+   elseif ($type == "GER light cruiser") { $objectdesc = "light cruiser"; }
+   elseif ($type == "FRpenicheAAA") { $objectdesc = "peniche AAA barge";}
+   elseif ($type == "GERpenicheAAA") { $objectdesc = "peniche AAA barge";}
+   else { $objectdesc = "unexpected ship type $type<br>"; }
 }
 ?>
