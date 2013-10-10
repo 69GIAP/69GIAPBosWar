c1b7e977d2f05c2edb9b0d5b2c25b6e013a532ca
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-10 14:51:33 -0400

    Make existing user an option (if one exists)
    Myata, here is my version of a solution.  It works, and does not force a
    choice either way.
    
    Your version has a myata_ prefix now so we don't lose it.

--- Website/CampaignCreateNew.php
+++ Website/CampaignCreateNew.php
@@ -13,6 +13,8 @@
 # Include the navigation on top
 	include ( 'includes/navigation.php' );
 
+# Check whether we already have defined campaign db user(s)
+$count = mysqli_query($dbc,"SELECT COUNT(*) FROM campaign_settings;");
 
 ?>
 
@@ -25,6 +27,7 @@
 				<?php
 					# start form
 					echo "<form id=\"airfieldForm\" name=\"login\" action=\"CampaignCreateConfirm.php\" method=\"post\">\n";
+//					echo "<form id=\"airfieldForm\" name=\"login\" action=\"testPost.php\" method=\"post\">\n";
 				
 					echo "	<fieldset id=\"inputs\">\n";	
 
@@ -35,48 +38,54 @@
 					echo "		<input type=\"text\" name=\"newCampaignDatabaseName\" id=\"database\" placeholder=\"Please enter the campaigns database name.\" value='' size=\"24\" maxlength=\"50\" />\n";
 					# NEW CAMPAIGN HOST
 					echo "		<input type=\"text\" name=\"newCampaignDatabaseHost\" id=\"database\" placeholder=\"Please enter the host (localhost / IP).\" value='' size=\"24\" maxlength=\"50\" />\n";	
-					echo "<h3>Database User</h3>\n";
-					
-					# check if there is already a user stored in the campaign_settings table
-					$query = "SELECT camp_user, camp_passwd FROM campaign_settings where camp_user != '' GROUP BY camp_user";
-					
-					if(!$result = $dbc->query($query))
-					{die('There was an error running the query [' . $dbc->error . ']');}
-				
-					if ($result = mysqli_query($dbc, $query)) 
-					{				
-						/* fetch associative array */
-						while ($obj = mysqli_fetch_object($result)) 
-							{
-								$campUser	=($obj->camp_user);
-								$campUserPW	=($obj->camp_passwd);
-							}
-						if (empty($campUser))
-						{
-							# NEW CAMPAIGN DATABASE USER
-							echo "		<input type=\"text\" name=\"newCampaignDatabaseUser\" id=\"username\" placeholder=\"Please enter the campaign DB user.\" value='' size=\"24\" maxlength=\"50\" />\n";	
-							# NEW CAMPAIGN DATABASE PASSWORD
-							echo "		<input type=\"text\" name=\"newCampaignDatabasePassword\" id=\"password\" placeholder=\"Please enter the campaign users password.\" value='' size=\"24\" maxlength=\"50\" />\n";
-						}
-						else
-						{
-							# CHOOSE CAMPAIGN MAP
-							echo "		<select name=\"newCampaignDatabaseUser\" id=\"username\">\n";
-							include 'includes/getDbUsers.php';
-							echo "		</select>\n";
-							echo "		<input type=\"hidden\" name=\"newCampaignDatabasePassword\" id=\"password\" value='' />\n";	
-						}
-					}
-					
-					
-				
-					echo "<h3>Campaign Map</h3>\n";
 					# CHOOSE CAMPAIGN MAP
+					echo "<h3>Campaign Map</h3>\n";
 					echo "		<select name=\"campaignMap\" id=\"world\">\n";
 					include 'includes/getCampaignMap.php';
 					echo "		</select>\n";
 				
+                                        if (!$count) {
+					   echo "<h3>Campaign User</h3>\n";
+					   # NEW CAMPAIGN DATABASE USER
+					   echo "		<input type=\"text\" name=\"newCampaignDatabaseUser\" id=\"username\" placeholder=\"Please enter the campaign DB user.\" value='' size=\"24\" maxlength=\"50\" />\n";	
+					   # NEW CAMPAIGN DATABASE PASSWORD
+					   echo "		<input type=\"text\" name=\"newCampaignDatabasePassword\" id=\"password\" placeholder=\"Please enter the campaign users password.\" value='' size=\"24\" maxlength=\"50\" />\n";
+					} else {
+					   echo "<h3>Either Create New Campaign User</h3>\n";
+					   # NEW CAMPAIGN DATABASE USER
+					   echo "		<input type=\"text\" name=\"newCampaignDatabaseUser\" id=\"username\" placeholder=\"Please enter the campaign DB user.\" value='' size=\"24\" maxlength=\"50\" />\n";	
+					   # NEW CAMPAIGN DATABASE PASSWORD
+					   echo "		<input type=\"text\" name=\"newCampaignDatabasePassword\" id=\"password\" placeholder=\"Please enter the campaign users password.\" value='' size=\"24\" maxlength=\"50\" />\n";
+$query = "SELECT `camp_host`, `camp_user`, `camp_passwd` from `campaign_settings` WHERE `camp_user` != '';";
+					   echo "<p><h3>OR select an existing one</h3></p>\n";
+					   echo "<p>Note: existing users take precedence over new.</p>\n";
+
+// removing indentation for clarity
+if(!$result = $dbc->query($query)) {
+	die('There was an error running the query [' . $dbc->error . ']');
+}
+mysqli_free_result($result);	
+
+if ($result = mysqli_query($dbc, $query)) {				
+	echo "<select id = \"existing\" name = \"existing\"><br>\n";
+	echo "<option value=\"\" selected>Select existing campaign user </option>\n";	
+	/* fetch result array */
+	while ($obj = mysqli_fetch_object($result)) {
+		$host = ($obj->camp_host);
+		$user = ($obj->camp_user);
+		$passwd	=($obj->camp_passwd);
+		// note hack to pass three variables as one  :)
+		echo "<option value=\"".$host."+".$user."+".$passwd."\">".$user."</option>\n";
+	}
+	echo "</select>\n";	
+}
+		
+mysqli_free_result($result);
+// we now return to our original indentation
+
 					echo "	</fieldset>\n";	
+
+					}
 				
 					# BUTTON	
 					echo "<fieldset id=\"actions\">\n";
