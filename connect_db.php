16b76d6344ceaf5e839c179f4bb38978ad4fca07
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-11 12:08:17 -0400

    add create_boswar_hq_user.sql
    To create a user for the boswar_db with what may be minimal privileges
    for what it needs to do.
    Please give it a try and report any issues.
    
    I've also revised connect_db.php to use this user once created.
    
    I've done some quick testing of this user and it is able to create new
    campaigns and parse logs.

--- connect_db.php
+++ connect_db.php
@@ -1,10 +1,11 @@
 
 <?php
-# Connect on '10.0.0.57:3306' for user 'manager' 
-# with password 'manager' to database 'boswar_db'.
+# Connect on 'localhost' (with configured default port) for user 'boswar_hq' 
+# with password 'boswar_password' to database 'boswar_db'.
 $dbc = mysqli_connect 
+  ( 'localhost' , 'boswar_hq', 'boswar_password', 'boswar_db' )
 # ( 'localhost' , 'peter' , 'grobble5' , 'boswar_db' )
-	( '10.0.0.57' , 'manager' , 'manager' , 'boswar_db' )
+# ( '10.0.0.57' , 'manager' , 'manager' , 'boswar_db' )
 OR die 
  ( mysqli_connect_error() ) ;
 # Set encoding to match PHP script encoding.
