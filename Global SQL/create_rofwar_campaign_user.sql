bf7cb33c43a2c062a69c9d859bd1734bdfa0988e
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-11 00:36:25 -0400

    update create_rofwar_campaign_user.sql
    Used incorrect comment style and had wrong username and password and
    insufficient permissions to undo stats.  But other than those,
    everything was right in the earlier version.  It just didn't work.  :)
    
    This one has been tested and verified to not only create the user and
    grand permissions but those permissions are sufficient for the parser.
    I haven't tested other things.

--- Global SQL/create_rofwar_campaign_user.sql
+++ Global SQL/create_rofwar_campaign_user.sql
@@ -1,13 +1,13 @@
-// create_rofwar_campaign_user.sql
-// =69.GIAP=TUSHKA
-// Oct 10, 2012
-// run this only after importing the four example campaign databases
-// bloody_april, flanders_eagles, skies_of_the_empires and 1916.
-// if one or more is missing you will get errors.
-// or comment out the ones you did not import before running this.
-CREATE USER 'boswar'@'localhost' IDENTIFIED BY 'boswar';
-GRANT FILE ON *.* TO 'boswar'@'localhost';
-GRANT ALL ON `bloody_april`.* TO 'boswar'@'localhost';
-GRANT ALL ON `flanders_eagles`.* TO 'boswar'@'localhost';
-GRANT ALL ON `skies_of_the_empires`.* TO 'boswar'@'localhost';
-GRANT ALL ON `1916`.* TO 'boswar'@'localhost';
+/* 
+   run this after importing the four example campaign databases 
+   =69.GIAP=TUSHKA
+   Oct 11, 2013
+*/
+
+CREATE USER 'rofwar'@'localhost' IDENTIFIED BY 'rofwar';
+GRANT FILE ON *.* TO 'rofwar'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON bloody_april.* TO 'rofwar'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON flanders_eagles.* TO 'rofwar'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON skies_of_the_empires.* TO 'rofwar'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `1916`.* TO 'rofwar'@'localhost';
+FLUSH PRIVILEGES;
