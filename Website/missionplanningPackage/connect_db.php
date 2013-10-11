033ea87aef15382014fa586bc08dc994e7870dd6
=69.GIAP=MYATA <rowbajt@gmail.com>
2013-10-11 21:46:16 +0200

    obsolete files

--- Website/missionplanningPackage/connect_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$camp_link=mysqli_connect
-('localhost','peter','bartok','stalingrad1_db')
-OR die
-(mysqli_connect_error());
-mysqli_set_charset($camp_link,'utf8');
--- Website/missionplanningPackage/require.php
+++
@@ -1,7 +0,0 @@
-<?php
-# incorporate connection script
-require ('../connect_db.php');
-if( mysqli_ping( $camp_link ))
-{ echo 'MySQL Server'.mysqli_get_server_info($camp_link).
-		'on'.mysqli_get_host_info($camp_link);}
-		
