6cd81567644f819d905162748c3856eedec61a9f
=69.GIAP=MYATA <rowbajt@gmail.com>
2013-10-11 21:40:39 +0200

    updates that should help STENKA to get his website up

--- Global SQL/campaign_settings.sql
+++ Global SQL/campaign_settings.sql
@@ -1,34 +1,32 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
+CREATE DATABASE  IF NOT EXISTS `boswar_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `boswar_db`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 08, 2013 at 07:10 PM
--- Server version: 5.6.11
--- PHP Version: 5.5.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: 10.0.0.57    Database: boswar_db
+-- ------------------------------------------------------
+-- Server version	5.6.13
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `boswar_db`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `campaign_settings`
 --
 
-CREATE TABLE IF NOT EXISTS `campaign_settings` (
+DROP TABLE IF EXISTS `campaign_settings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `campaign_settings` (
   `id` smallint(1) unsigned NOT NULL AUTO_INCREMENT,
-  `simulation` enum('RoF','BoS') NOT NULL,
+  `simulation` varchar(6) NOT NULL,
   `campaign` varchar(30) NOT NULL,
   `camp_db` varchar(30) NOT NULL,
   `camp_host` varchar(30) NOT NULL,
@@ -36,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `campaign_settings` (
   `camp_passwd` varchar(30) NOT NULL,
   `map` varchar(30) NOT NULL,
   `map_locations` varchar(40) NOT NULL,
-  `status` enum('1','2','3','4') NOT NULL DEFAULT '4',
+  `status` tinyint(1) NOT NULL,
   `show_airfield` tinyint(1) NOT NULL,
   `finish_flight_only_landed` tinyint(1) NOT NULL,
   `logpath` varchar(60) NOT NULL,
@@ -55,36 +53,21 @@ CREATE TABLE IF NOT EXISTS `campaign_settings` (
   `healthy` smallint(1) NOT NULL DEFAULT '0',
   `min_x` mediumint(1) NOT NULL,
   `min_z` mediumint(1) NOT NULL,
-  `max_x` mediumint(1) NOT NULL,
-  `max_z` mediumint(1) NOT NULL,
-  `air_detect_distance` smallint(1) unsigned NOT NULL DEFAULT '5000',
-  `ground_detect_distance` smallint(1) unsigned NOT NULL DEFAULT '500',
-  `air_ai_level` enum('1','2','3') NOT NULL DEFAULT '2',
-  `ground_ai_level` enum('1','2','3') NOT NULL DEFAULT '2',
-  `ground_max_speed_kmh` tinyint(1) unsigned NOT NULL DEFAULT '50',
-  `ground_transport_speed_kmh` tinyint(1) unsigned NOT NULL DEFAULT '10',
-  `ground_spacing` tinyint(1) unsigned NOT NULL DEFAULT '5',
-  `lineup_minutes` tinyint(1) unsigned NOT NULL DEFAULT '30',
-  `mission_minutes` tinyint(1) unsigned NOT NULL DEFAULT '90',
+  `max_x` mediumint(9) NOT NULL,
+  `max_z` mediumint(9) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `campaign` (`campaign`),
   UNIQUE KEY `camp_db` (`camp_db`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=37 ;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Dumping data for table `campaign_settings`
---
-
-INSERT INTO `campaign_settings` (`id`, `simulation`, `campaign`, `camp_db`, `camp_host`, `camp_user`, `camp_passwd`, `map`, `map_locations`, `status`, `show_airfield`, `finish_flight_only_landed`, `logpath`, `log_prefix`, `logfile`, `kia_pilot`, `mia_pilot`, `critical_w_pilot`, `serious_w_pilot`, `light_w_pilot`, `kia_gunner`, `mia_gunner`, `critical_w_gunner`, `serious_w_gunner`, `light_w_gunner`, `healthy`, `min_x`, `min_z`, `max_x`, `max_z`, `air_detect_distance`, `ground_detect_distance`, `air_ai_level`, `ground_ai_level`, `ground_max_speed_kmh`, `ground_transport_speed_kmh`, `ground_spacing`, `lineup_minutes`, `mission_minutes`) VALUES
-(2, 'RoF', 'Flanders Eagles', 'flanders_eagles', 'localhost', 'rofwar', 'rofwar', 'Channel', 'rof_channel_locations', '2', 0, 0, '', '', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2', '2', 0, 0, 0, 30, 90),
-(3, 'RoF', 'Lake', 'lake', '', '', '', 'Lake', 'rof_lake_locations', '4', 0, 0, '', '', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2', '2', 0, 0, 0, 30, 90),
-(4, 'RoF', 'Skies of the Empires II', 'skies_of_the_empires_ii', '', '', '', 'Verdun', 'rof_verdun_locations', '2', 0, 0, '', '', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2', '2', 0, 0, 0, 30, 90),
-(5, 'BoS', 'Stalingrad', 'stalingrad', '', '', '', 'Stalingrad', 'bos_stalingrad_locations', '4', 0, 0, '', '', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2', '2', 0, 0, 0, 30, 90),
-(6, 'RoF', 'Yankee Doodle', 'yankee_doodle', '', '', '', 'Verdun', 'rof_verdun_locations', '2', 0, 0, '', '', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2', '2', 0, 0, 0, 30, 90),
-(7, 'RoF', 'Skies of the Empires', 'skies_of_the_empires', 'localhost', 'rofwar', 'rofwar', 'Verdun', 'rof_verdun_locations', '3', 0, 0, '', '', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2', '2', 0, 0, 0, 30, 90),
-(8, 'RoF', '1916', '1916', 'localhost', 'rofwar', 'rofwar', 'Western Front', 'rof_westernfront_locations', '3', 0, 0, '', '', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2', '2', 0, 0, 0, 30, 90),
-(9, 'RoF', 'Bloody April', 'bloody_april', 'localhost', 'rofwar', 'rofwar', 'Western Front', 'rof_westernfront_locations', '1', 0, 0, '', '', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '2', '2', 0, 0, 0, 30, 90);
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-10-11 21:40:17
