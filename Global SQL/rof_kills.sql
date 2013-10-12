136804bfd2047053d7b5c908a11a0945864714f7
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-11 23:41:13 -0400

    found another... rof_kills
    Nuking nulls!

--- Global SQL/rof_kills.sql
+++ Global SQL/rof_kills.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2013 at 04:43 AM
+-- Generation Time: Oct 12, 2013 at 05:39 AM
 -- Server version: 5.6.11
 -- PHP Version: 5.5.3
 
@@ -29,20 +29,20 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `rof_kills`;
 CREATE TABLE IF NOT EXISTS `rof_kills` (
   `id` smallint(1) unsigned NOT NULL AUTO_INCREMENT,
-  `MissionID` varchar(60) NOT NULL,
-  `clocktime` time NOT NULL,
-  `attackerID` mediumint(1) NOT NULL,
-  `attackerName` varchar(50) NOT NULL,
-  `attackerCountryID` smallint(1) NOT NULL,
-  `attackerCoalID` tinyint(1) unsigned NOT NULL,
-  `action` varchar(20) NOT NULL,
-  `targetID` mediumint(1) NOT NULL,
-  `targetClass` varchar(8) NOT NULL,
-  `targetType` varchar(50) NOT NULL,
-  `targetName` varchar(50) NOT NULL,
-  `targetCountryID` smallint(1) unsigned NOT NULL,
-  `targetCoalID` tinyint(1) unsigned NOT NULL,
-  `targetValue` smallint(1) NOT NULL,
+  `MissionID` varchar(60) NOT NULL DEFAULT 'missionid',
+  `clocktime` time NOT NULL DEFAULT '00:00:00',
+  `attackerID` mediumint(1) NOT NULL DEFAULT '0',
+  `attackerName` varchar(50) NOT NULL DEFAULT 'attacker name',
+  `attackerCountryID` smallint(1) NOT NULL DEFAULT '0',
+  `attackerCoalID` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `action` varchar(20) NOT NULL DEFAULT 'action',
+  `targetID` mediumint(1) NOT NULL DEFAULT '0',
+  `targetClass` varchar(8) NOT NULL DEFAULT 'xxx',
+  `targetType` varchar(50) NOT NULL DEFAULT 'target type',
+  `targetName` varchar(50) NOT NULL DEFAULT 'target name',
+  `targetCountryID` smallint(1) unsigned NOT NULL DEFAULT '0',
+  `targetCoalID` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `targetValue` smallint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
