13e4f382333a749c34d183673d754673d1b2b208
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-11 23:02:41 -0400

    preventing nulls in campaign_users
    Not that this was a problem, but just to be on the safe side.

--- Global SQL/campaign_users.sql
+++ Global SQL/campaign_users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 24, 2013 at 05:48 PM
+-- Generation Time: Oct 12, 2013 at 05:01 AM
 -- Server version: 5.6.11
 -- PHP Version: 5.5.3
 
@@ -28,23 +28,24 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `campaign_users`;
 CREATE TABLE IF NOT EXISTS `campaign_users` (
-  `user_id` int(11) NOT NULL,
-  `camp_db` varchar(30) NOT NULL,
-  `CoalID` int(2) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`camp_db`,`user_id`,`CoalID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='This table binds the entries of the user table to the campaigns.\nThis makes it possible to filter what campaign and what coalition the user is in.';
+  `id` smallint(1) NOT NULL AUTO_INCREMENT,
+  `user_id` smallint(1) NOT NULL DEFAULT '0',
+  `camp_db` varchar(30) NOT NULL DEFAULT 'campaign_database',
+  `CoalID` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `campaign_users`
 --
 
-INSERT INTO `campaign_users` (`user_id`, `camp_db`, `CoalID`) VALUES
-(1, '1916', 0),
-(1, 'flanders_eagles', 0),
-(2, 'flanders_eagles', 1),
-(4, 'flanders_eagles', 0),
-(1, 'skies_of_the_empires', 0),
-(2, 'skies_of_the_empires', 2);
+INSERT INTO `campaign_users` (`id`, `user_id`, `camp_db`, `CoalID`) VALUES
+(1, 1, '1916', 0),
+(2, 1, 'flanders_eagles', 0),
+(3, 2, 'flanders_eagles', 1),
+(4, 4, 'flanders_eagles', 0),
+(5, 1, 'skies_of_the_empires', 0),
+(6, 2, 'skies_of_the_empires', 2);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
