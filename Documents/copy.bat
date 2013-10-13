ded94ac1b7ee763568ce3eee1599b2cb75236a0e
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-13 15:07:15 -0400

    adding documents on configuring RoF for logs
    startup.cfg.settings.txt and copy.bat

---
+++ Documents/copy.bat
@@ -0,0 +1,5 @@
+copy /b "missionReport(*)[?].txt" missionReport1.txt
+copy /b "missionReport(*)[??].txt" missionReport2.txt
+copy /b "missionReport(*)[???].txt" missionReport3.txt
+copy /b "missionReport(*)[????].txt" missionReport4.txt
+copy /b missionReport?.txt missionReport.txt
