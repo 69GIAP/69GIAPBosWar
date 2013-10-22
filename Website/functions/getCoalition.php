31cd058180c79d9cf1f616409bcecb7186563f82
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-22 18:28:31 -0400

    add function getCoalition.php and matching test
    This is for Stenka, illustrating another structure and use of a
    function.
    Test it by running test_getCoalition.php from your browser.  Edit that
    file to
    see different countries.  (It is not a sophisticated test.  :) )
    
    This function returns a single value, the coalition's ID.

---
+++ Website/functions/getCoalition.php
@@ -0,0 +1,21 @@
+<?php
+// getCoalition
+// given country key #, get current Coalition ID
+// =69.GIAP=TUSHKA
+// Oct 22, 2013
+// version 1.0
+
+// define the function 
+function get_coalition($ckey) {
+global $camp_link; // link to campaign db
+
+   $query = "SELECT CoalID FROM rof_countries WHERE ckey = '$ckey'";
+   if($result = mysqli_query($camp_link, $query)) {
+      while ($obj = mysqli_fetch_object($result)) {
+	 return($obj->CoalID);
+      }
+   } else {
+      die('getCoalition query error [' . $camp_link->error . ']');
+   }
+}
+?>
