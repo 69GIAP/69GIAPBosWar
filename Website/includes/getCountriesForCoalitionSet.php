5e27113d03893b1aaf6a0bc33585a13c294b8c13
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-24 16:55:06 -0400

    update getCountriesForCoalitionSet
    Used $camp_link rather than $dbc, pre-select default coalition
    assignments, get coalition names from rof_coalitions table rather than
    hard-wire them (so Redvo can change them as he has already done.  :) )
    Try setup for the Skies of the Empires campaign to see how well this
    works now.

--- Website/includes/getCountriesForCoalitionSet.php
+++ Website/includes/getCountriesForCoalitionSet.php
@@ -1,16 +1,31 @@
 <?php
 // Get Countries and their coalitions
 // out of boswar_db
-// 69giapmyata
-// ver 1.0
+// 69giapmyata and =69.GIAP=TUSHKA
+// Oct 24, 2013
+// ver 1.1
 
-				
-	$sql = "SELECT countryname FROM rof_countries";
+// use $camp_link rather than $dbc
+// get coalition names from coalition table rather than hard-wiring them
+// coalitions 1 and 2 are all we will offer
+$query = "SELECT * FROM rof_coalitions";
+	if(!$result = $camp_link->query($query)){
+		die('There was an error running the query ' . mysqli_error($dbc));
+	}
+	while ($obj = mysqli_fetch_object($result)) {
+		if (($obj->CoalID) == 1 ) {
+			$coal_name1=($obj->Coalitionname);
+		} elseif (($obj->CoalID) == 2) {
+			$coal_name2 =($obj->Coalitionname);
+		}
+	}
+
+	$sql = "SELECT * FROM rof_countries WHERE ckey > 1 AND ckey < 600";
 	$i = 1;
 	
 	#echo $sql;
 	echo "		<!-- Checkboxes for country and coalition selection -->\n";
-	if(!$result = $dbc->query($sql)){
+	if(!$result = $camp_link->query($sql)){
 		die('There was an error running the query ' . mysqli_error($dbc));
 	}
 	
@@ -20,20 +35,30 @@
 	# load results into variables 
 	while ($obj = mysqli_fetch_object($result)) {
 		$countryName		=($obj->countryname);
+		$CoalID			=($obj->CoalID);
 		echo "<div class=\"checkbox\">\n";		
 		echo "		<input id=\"checkboxCountry$i\" type=\"checkbox\" name =\"add$i\" value =\"$countryName\">\n";
 		echo "		<label for=\"checkboxCountry$i\"><b>$countryName</b></label><br \>\n";
 		echo "</div>\n";		
 		# COALITION RADIO BOX
 		echo "<div class=\"radio\">\n";  
-		echo "	<input id=\"ententeCountry$i\" type=\"radio\" name=\"radiobox$i\" value=\"1\">  \n";
-		echo "	<label for=\"ententeCountry$i\">Entente</label>  \n";
+		if ($CoalID == 1){
+		echo "	<input id=\"ententeCountry$i\" type=\"radio\" name=\"radiobox$i\" value=\"1\" checked=\"checked\">  \n";
+		echo "	<label for=\"ententeCountry$i\">$coal_name1</label>  \n";
 		echo "	<input id=\"centerCountry$i\" type=\"radio\" name=\"radiobox$i\" value=\"2\">  \n";
-		echo "	<label for=\"centerCountry$i\">Center</label> \n"; 
+		echo "	<label for=\"centerCountry$i\">$coal_name2</label> \n"; 
+		} else {
+		echo "	<input id=\"ententeCountry$i\" type=\"radio\" name=\"radiobox$i\" value=\"1\">  \n";
+		echo "	<label for=\"ententeCountry$i\">$coal_name1</label>  \n";
+		echo "	<input id=\"centerCountry$i\" type=\"radio\" name=\"radiobox$i\" value=\"2\" checked=\"checked\">  \n";
+		echo "	<label for=\"centerCountry$i\">$coal_name2</label> \n"; 
+		}
+
 		echo "</div>\n";
 	$i ++;
 	}
 	echo "</div>\n";
-
+	// free result set
+	mysqli_free_result($result);
 ?>
 
