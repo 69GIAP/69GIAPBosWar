9dac881c23f4a6a8ff10fe3865e320d716876ea7
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-26 00:44:38 -0400

    updated uploadFile.php
    Got it working with .Group and .Mission files (previously it only loaded
    graphics files)... and *restricted* it to .Group and .Mission files
    because that is all our commanders or admins will need to upload.  I
    also increased the max file size from 2 MB to 4 MB because some of our
    Flanders Eagles .Mission files are over 3 MB.

--- Website/uploadFile.php
+++ Website/uploadFile.php
@@ -1,79 +1,63 @@
 <?php 
+// uploadFile.php
+// upload .Group and .Mission files to server
+// =69.GIAP=MYATA and =69.GIAP=TUSHKA
+// Oct 11, 2013
+// latest revision: Oct 26, 2013
+// BOSWAR version1.1
 
-# Incorporate the MySQL connection script.
-	require ( '../connect_db.php' );
+// Incorporate the MySQL connection script.
+require ( '../connect_db.php' );
 	
-# Include the webside header
-	include ( 'includes/header.php' );
+// Include the webside header
+include ( 'includes/header.php' );
 	
-# Include the navigation on top
-	include ( 'includes/navigation.php' );
+// Include the navigation on top
+include ( 'includes/navigation.php' );
 
 ?>
-
-	<div id="wrapper">
-
-        <div id="container">
-    
-            <div id="content">
-				<?php           
-                    $allowedExts = array("gif", "jpeg", "jpg", "png");
-                    $temp = explode(".", $_FILES["file"]["name"]);
-                    $extension = end($temp);
-                    if	(
-                            (
-                                    ($_FILES["file"]["type"] == "image/gif")
-                                ||	($_FILES["file"]["type"] == "image/jpeg")
-                                ||	($_FILES["file"]["type"] == "image/jpg")
-                                ||	($_FILES["file"]["type"] == "image/pjpeg")
-                                ||	($_FILES["file"]["type"] == "image/x-png")
-                                ||	($_FILES["file"]["type"] == "image/png")
-                            )
-                                && ($_FILES["file"]["size"] < 2000000)
-                                && in_array($extension, $allowedExts)
-                        )
-                      {
-                      if ($_FILES["file"]["error"] > 0)
-                        {
-                        echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-                        }
-                      else
-                        {
-                        echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-                        echo "Type: " . $_FILES["file"]["type"] . "<br>";
-                        echo "Size: " . (round ($_FILES["file"]["size"] / 1024 /1024, 2)) . " MB<br>";
-                        echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
-                    
-                        if (file_exists("C:/BOSWAR/" . $_FILES["file"]["name"]))
-                          {
-                          echo $_FILES["file"]["name"] . " already exists. ";
-                          }
-                        else
-                          {
-                          move_uploaded_file($_FILES["file"]["tmp_name"],
-                          "C:/BOSWAR/" . $_FILES["file"]["name"]);
-                          echo "Stored in: " . "C:/BOSWAR/" . $_FILES["file"]["name"];
-                          }
-                        }
-                      }
-                    else
-                      {
-                      echo "Invalid file";
-                      }
-                ?> 
-		</div>
-    
-	</div>
-
+<div id="wrapper">
+<div id="container">
+<div id="content">
+<?php           
+// restrict uploaded files to .Group and .Mission files
+$allowedExts = array("Group", "Mission");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+// limit size to 4 MB max (3 MB is a large .Mission file in RoF)
+// (can tune later if needed for BoS)
+// and require extension to be in allowed list
+if ( $_FILES["file"]["size"] < 4000000 && in_array($extension, $allowedExts)) {
+   if ($_FILES["file"]["error"] > 0) {
+      echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+   } else {
+      echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+      echo "Type: " . $_FILES["file"]["type"] . "<br>";
+      echo "Size: " . (round ($_FILES["file"]["size"] / 1024 /1024, 2)) . " MB<br>";
+      echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+      if (file_exists("C:/BOSWAR/" . $_FILES["file"]["name"])) {
+	 echo $_FILES["file"]["name"] . " already exists. ";
+      } else {
+	 move_uploaded_file($_FILES["file"]["tmp_name"],
+	 "C:/BOSWAR/" . $_FILES["file"]["name"]);
+	 echo "Saved to: " . "C:/BOSWAR/" . $_FILES["file"]["name"];
+      }
+   }
+} else {
+   echo "Invalid file";
+}
+?> 
+</div>
+</div>
 <?php
-	# Include the general sidebar
-	include ( 'includes/sidebar.php' );
+// Include the general sidebar
+include ( 'includes/sidebar.php' );
 ?>	
 
-		<div id="clearing"></div>
-	</div>
+<div id="clearing"></div>
+</div>
 
 <?php
-	# Include the footer
-	include ( 'includes/footer.php' );
+// Include the footer
+include ( 'includes/footer.php' );
 ?>
