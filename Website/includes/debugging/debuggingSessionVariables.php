b926f7865a000f6c52e68176ef4c61c52208e7ef
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-27 18:09:22 -0400

    update debuggingSessionVariables.php
    Just show "$camp_link" if passing that mysqli object at a Session
    variable.

--- Website/includes/debugging/debuggingSessionVariables.php
+++ Website/includes/debugging/debuggingSessionVariables.php
@@ -2,6 +2,11 @@
 	# debugging SESSION variables
 	echo "\n <div class=\"debuggingSession\">\n";
 	foreach ($_SESSION as $param_name => $param_val) {
-		echo "\$_SESSION $param_name: <b>$param_val</b><br> \n";}
+		if ($param_name == 'camp_link') {
+			echo "\$_SESSION $param_name: <b>\$$param_name</b><br> \n";
+		} else {
+			echo "\$_SESSION $param_name: <b>$param_val</b><br> \n";
+		}
+	}
 	echo "</div> \n";
 ?>	
