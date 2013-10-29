7fa6620d399330043a8f2f4aa236c5172657e3db
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-28 23:51:44 -0400

    update outputTURRETGUNNER.php
    convert mysqli functions too object oriented style and reformat.

--- Website/rof_parser/functions/outputTURRETGUNNER.php
+++ Website/rof_parser/functions/outputTURRETGUNNER.php
@@ -2,28 +2,27 @@
 // TURRETGUNNER
 // given linenumber of a player get object_class and object_desc
 // =69.GIAP=TUSHKA
-// BOSWAR version 1.2
-// Oct 17, 2013
-// now seems to be misnamed.  :)
-// starting to think of player as just another object.
+// 2011-2013
+// BOSWAR version 1.3
+// Oct 28, 2013
 
 function TURRETGUNNER($j){
-   global $TYPE; // type of plane, object, or objective - primary or secondary
-   global $camp_link; // link to campaign db
-   global $object_class; // object class from rof_object_properties
-   global $object_desc; // object description from rof_object_properties
+	global $TYPE; // type of plane, object, or objective - primary or secondary
+	global $camp_link; // link to campaign db
+	global $object_class; // object class from rof_object_properties
+	global $object_desc; // object description from rof_object_properties
 
-   $query = "SELECT object_class, object_desc FROM rof_object_properties WHERE object_type = '$TYPE[$j]'";
-   if ($result = mysqli_query($camp_link, $query)) {
-      // get results
-      while ($obj = mysqli_fetch_object($result)) {
-      $object_class	= ($obj->object_class);
-      $object_desc	= ($obj->object_desc);
-   }
-      // free result set
-      mysqli_free_result($result); 
-   } else { 
-      die('There was an error running the query [' . $camp_link->error . ']');
-   }
+	$query = "SELECT object_class, object_desc FROM rof_object_properties WHERE object_type = '$TYPE[$j]'";
+	if ($result = $camp_link->query($query)) {
+		// get results
+		while ($obj = $result->fetch_object()) {
+			$object_class	= ($obj->object_class);
+			$object_desc	= ($obj->object_desc);
+		}
+		// free result set, object oriented style
+		$result->close(); 
+} else { 
+		die('TURRETGUNNER query entry [' . $camp_link->error . ']');
+	}
 }
 ?>
