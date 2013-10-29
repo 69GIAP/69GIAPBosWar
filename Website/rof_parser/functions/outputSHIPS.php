fec3549ed5ae7965785e2c8251a043fc00607599
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-28 23:38:49 -0400

    update parser function  outputSHIPS.php
    convert mysqli functions to object oriented style and reformat

--- Website/rof_parser/functions/outputSHIPS.php
+++ Website/rof_parser/functions/outputSHIPS.php
@@ -1,24 +1,25 @@
 <?php
 // SHIPS
 // translate ship type into more natural description
-// BOSWAR version 1.3
-// Oct 17, 2013
+// 2013
+// BOSWAR version 1.4
+// Oct 28, 2013
 function SHIPS($type) {
-   global $camp_link; // link to campaign db
-   global $object_class; // object class from rof_object_properties
-   global $object_desc; // object description from rof_object_properties
+	global $camp_link; // link to campaign db
+	global $object_class; // object class from rof_object_properties
+	global $object_desc; // object description from rof_object_properties
 
-   $query = "SELECT object_class, object_desc FROM rof_object_properties WHERE object_type = '$type'";
-   if ($result = mysqli_query($camp_link, $query)) {
-      // get results
-      while ($obj = mysqli_fetch_object($result)) {
-      $object_class	= ($obj->object_class);
-      $object_desc	= ($obj->object_desc);
-   }
-      // free result set
-      mysqli_free_result($result); 
-   } else { 
-      die('There was an error running the query [' . $camp_link->error . ']');
-   }
+	$query = "SELECT object_class, object_desc FROM rof_object_properties WHERE object_type = '$type'";
+	if ($result = $camp_link->query($query)) {
+		// get results
+		while ($obj = $result->fetch_object($result)) {
+			$object_class	= ($obj->object_class);
+			$object_desc	= ($obj->object_desc);
+		}
+		// free result set, object oriented style
+		$result->close(); 
+	} else { 
+		die('There was an error running the query [' . $camp_link->error . ']');
+	}
 }
 ?>
