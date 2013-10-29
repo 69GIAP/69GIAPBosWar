2030ff8b910634df17ffdd6f284eefd1b94c4e09
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-29 15:07:21 -0400

    new includes getCampaignVariables.php
    Extracted from CampaignMgmtStatics.php for use in all those other pages
    that do the same thing.  :)

--- Website/CampaignMgmtStatics.php
+++ Website/CampaignMgmtStatics.php
@@ -21,40 +21,15 @@
 				<?php
 					# include connect2CampaignFunction.php
 					include ( 'functions/connect2Campaign.php' );
+
+					# include getCampaignVariables.php
+					include ( 'includes/getCampaignVariables.php' );
 		
-					# use it to get remaining variables
-					$query = "SELECT * from campaign_settings where camp_db = '$loadedCampaign'";  
-		 
-					if(!$result = $dbc->query($query)) {
-						die('There was an error running the query [' . $dbc->error . ']');
-					}
-		
-					if ($result = $dbc->query($query)) {
-						/* fetch associative array */
-						while ($obj = $result->fetch_object()) {
-							$campaign	=($obj->campaign);
-							$camp_host	=($obj->camp_host);
-							$camp_user	=($obj->camp_user);
-							$camp_passwd=($obj->camp_passwd);
-							$camp_status_id=($obj->status);
-							
-							# get campaign status
-							$sql="SELECT campaign_status FROM campaign_status where id = $camp_status_id";
-							if ($result = $dbc->query($sql)) {
-							/* fetch associative array */
-							while ($obj = $result->fetch_object()) {
-								$camp_status=($obj->campaign_status);
-								}
-							}
-						}
-					} 
-					# free result set
-					$result->close();
-									
 					# use this information to connect to campaign 
 					$camp_link = connect2campaign("$camp_host","$camp_user","$camp_passwd","$loadedCampaign");
 
 					# useful stuff goes here
+					
 					# close $camp_link
 					$camp_link->close();
                 ?>
