9a7e82b81e79566190d1d20d07eb6fa2a8f7060b
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-29 00:03:17 -0400

    update parser function processOBJECTPROPERTIES.php
    convert mysqli functions to object oriented style and reformat

--- Website/rof_parser/functions/processOBJECTPROPERTIES.php
+++ Website/rof_parser/functions/processOBJECTPROPERTIES.php
@@ -1,10 +1,11 @@
 <?php
 // OBJECTPROPERTIES
-// =69.GIAP=TUSHKA
-// ver 1.1
-// Oct 17, 2013
 // get object's properties from rof_object_properties
 // called from processLASTHIT and coreOUTPUT
+// =69.GIAP=TUSHKA
+// 2013
+// ver 1.2
+// Oct 28, 2013
 
 function OBJECTPROPERTIES($objecttype) {
    global $camp_link; // link to campaign db
@@ -14,30 +15,26 @@ function OBJECTPROPERTIES($objecttype) {
    
    // our query to the campaign db
    $query = "SELECT * from rof_object_properties where object_type = '$objecttype'";
-
-   // get result and count
-   $result = mysqli_query($camp_link, $query);
-   $count = mysqli_num_rows($result);
-
-   // use result (should be just one)
-   if ($count == 1) {
-	// use result or complain
-	$row = mysqli_fetch_row($result);
-	$object_class = $row[2];
-	$object_value = $row[3];
-	$object_desc = $row[4];
-   } elseif ($count > 1) {
-	echo "duplicate $objecttype found in rof_object_properties!<br>\n";
-   } else { // count must be zero
-        echo "$objecttype not found in rof_object_properties!<br>\n";
-   }
-
-   // close result set
-   mysqli_free_result($result);
-   
-//   debugging
-//   echo "\$object_class = $object_class, \$object_value = $object_value, \$object_desc = $object_desc<br?\n";
-
+	if ($result = $camp_link->query($query)) {
+		$count = mysqli_num_rows($result);
+		// use result (should be just one)
+		if ($count == 1) {
+			// get results
+			// use result or complain
+			$row = $result->fetch_row();
+			$object_class = $row[2];
+			$object_value = $row[3];
+			$object_desc = $row[4];
+		} elseif ($count > 1) {
+			echo "duplicate $objecttype found in rof_object_properties!<br>\n";
+		} else { // count must be zero
+			echo "$objecttype not found in rof_object_properties!<br>\n";
+		}
+		// close result set
+		$result->close();
+	} else { 
+		die('BOTGUNNER query error [' . $camp_link->error . ']');
+	}
 }
 ?>
 
