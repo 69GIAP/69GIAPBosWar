fcacad3ae7631dfd4803320d2ca078ab319e1d6b
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-29 23:10:59 -0400

    update CampaignMgmtChangeStatus.php
    Use getCampaignVariables.php and close both $dbc and $camp_link when
    done.

--- Website/CampaignMgmtChangeStatus.php
+++ Website/CampaignMgmtChangeStatus.php
@@ -9,7 +9,6 @@
 # Include the navigation on top
 	include ( 'includes/navigation.php' );
 
-
 ?>
 
 	<div id="wrapper">
@@ -21,34 +20,10 @@
 				<?php
 					# include connect2CampaignFunction.php
 					include ( 'functions/connect2Campaign.php' );
-		
-					# use it to get remaining variables
-					$query = "SELECT * from campaign_settings where camp_db = '$loadedCampaign'";  
-		 
-					if(!$result = $dbc->query($query)) {
-						die('There was an error running the query [' . $dbc->error . ']');
-					}
-		
-					if ($result = mysqli_query($dbc, $query)) {
-						/* fetch associative array */
-						while ($obj = mysqli_fetch_object($result)) {
-							$campaign	=($obj->campaign);
-							$camp_host	=($obj->camp_host);
-							$camp_user	=($obj->camp_user);
-							$camp_passwd=($obj->camp_passwd);
-							$camp_status_id=($obj->status);
-							
-							# get campaign status
-							$sql="SELECT campaign_status FROM campaign_status where id = $camp_status_id";
-							if ($result = mysqli_query($dbc, $sql)) {
-							/* fetch associative array */
-							while ($obj = mysqli_fetch_object($result)) {
-								$camp_status=($obj->campaign_status);
-								}
-							}
-						}
-					} 
-									
+
+					# include getCampaignVariables.php
+					include ( 'includes/getCampaignVariables.php' );
+
 					# use this information to connect to campaign 
 					$camp_link = connect2campaign("$camp_host","$camp_user","$camp_passwd","$loadedCampaign");
 					
@@ -69,6 +44,9 @@
 
 					echo "</form>\n";          
 
+					# close $camp_link
+					$camp_link->close();
+
                 ?>
             
             </div>
@@ -84,6 +62,9 @@
 	</div>
 
 <?php
+	# close $dbc
+	$dbc->close();
+
 	# Include the footer
 	include ( 'includes/footer.php' );
 ?>
