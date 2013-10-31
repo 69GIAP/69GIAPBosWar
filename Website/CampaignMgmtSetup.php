5a8e7c0773e529235f72e4c1ef4ca3406fd10e1f
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-10-31 15:52:08 -0400

    update CampaignMgmtSetup.php
    Added and tested the section on configuring Influence Areas in the
    mission editor.

--- Website/CampaignMgmtSetup.php
+++ Website/CampaignMgmtSetup.php
@@ -33,7 +33,7 @@
 					echo "<form id=\"campaignMgmtSetupForm\" name=\"campaignSetup\" action=\"CampaignMgmtSetupConfirm.php?btn=campMgmt\" method=\"post\">\n";
 					
 					echo "<br>Next we work on setting up the campaign in the mission editor.<br />\n";
-					echo "<br>You can open the Mission Editor in a separate window to carry out this process.<br>\n";
+					echo "<br>You can open the mission editor in a separate window to carry out this process.<br>\n";
 					
 					echo "<h3>Preliminaries</h3>\n";
 					echo "<p>To start a new mission click 'File' and 'New' in the upper left.</p>\n";
@@ -44,9 +44,9 @@
 					echo "<h3>The Campaign Map</h3>\n";
 					echo "<p>Now we must select the Map. Currently in ROF there are two main campaign maps, the <b>Western Front map (05.01.18, etc)</b> and the <b>Channel map</b>.  Then there are also two small dogfight maps: <b>df3xlake</b> and <b>df5x5verdun</b>.</p>\n";
 					echo "<p>Select whichever GUI map and season best suits your campaign.</p>\n";
-					echo "<p>Then for Landscape info, Height Map, Textures, and Forests you need to select an appropriate matching set in the Mission editor.<br>\n"; 
+					echo "<p>Then for Landscape info, Height Map, Textures, and Forests you need to select an appropriate matching set in the mission editor.<br>\n"; 
 					$campaign_template = "$campaign"."_template";
-					echo "<p>In the Mission Editor File menu, select 'Save As...', giving it a file name for the campaign plus\"_template\" (e.g. $campaign_template.Mission) and placing it in the /data/Multiplayer/Dogfight/ directory.</p>\n";
+					echo "<p>In the mission editor File menu, select 'Save As...', giving it a file name for the campaign plus\"_template\" (e.g. $campaign_template.Mission) and placing it in the /data/Multiplayer/Dogfight/ directory.</p>\n";
 					// we should be able to determine the map from the GuiMap line in the Options section of the Mission file... just a SMOP.  :)
 					/*
 					echo "<p>Next we have  to tell which map was chosen to our campaign manager so select the map button you have chosen:</p>\n";	
@@ -68,11 +68,11 @@
 					*/
 
 					echo "<h3>The Opposing Sides</h3>\n";
-					echo "<p>We now need to define who is fighting whom. So back in the Mission editor in Mission Properties, click on Countries.</p>\n";
-					echo "<p>Our Campaign manager has been designed to create a war between two coalitions, Allies (Entente) and Central Powers.<br>\n";
+					echo "<p>We now need to define who is fighting whom. So back in the mission editor in 'Mission Properties', click on 'Countries'.</p>\n";
+					echo "<p>Our campaign manager has been designed to create a war between two coalitions, Allies (Entente) and Central Powers.<br>\n";
 					echo "While it is possible to configure other theoretical alliances like \"War dogs\" and \"Mercenaries\"<br>\n";
 					echo "We did not design or test any options other than Allies (Entente) and Central Powers so allocate the real countries to either coalition and ignore the rest.<br>\n";
-					echo "In the Mission Editor you should use the File menu, Save before coming back here.</p>\n";
+					echo "In the mission editor you should use the File menu, Save, before coming back here.</p>\n";
 					
 //					echo "	<fieldset id=\"inputs\">\n";
 //					# The input from this section will update the countries/coalition table for the campaign
@@ -92,12 +92,12 @@
 					*/
 					
 					echo "<h3>Import the infrastructure</h3>\n";
-					echo "<p>This is the slow step.</p>\n";
 					echo "<p>We will now populate our template with all defined infrastructure (including, most importantly, the airfields).</p>\n" ;
 					echo "<p>In the mission editor 'File' menu select 'Import From File...', go to: <b>directory /data/Template/</b></p>\n";
 					echo "<p>Select the base-no-trunc file that corresponds to your map. </p>\n";
 					echo "<p>The Western front map uses Base-no-trunc the Channel map uses base-no-trunc-channel the dogfight maps have merged base files. \n";
-					echo "Loading in these files can take a while, be patient. Refresh your beverage, relieve yourself, check the forums, watch several yourtube videos, and generally wait until the 'Please wait until operation is finished' popup disappears.  This seemingly takes forever (tens of minutes on a reasonably fast system) for the large maps... and loading a mission would take as long if we left all that stuff in the map, which, of course, we will not do.  This should impress you with the detailed work that has gone into creating these maps!</p>\n";
+					echo "Loading in these files can take a while, be patient. Refresh your beverage, relieve yourself, check the forums, watch several yourtube videos, and generally wait until the 'Please wait until operation is finished' popup disappears. If the load hangs near the end, quit and reload the file which should now be a quick process.</p>\n";
+					echo "<p>Save the mission before continuing.</p>\n";
 					
 //					echo "In the Mission Editor you should use the file menu, save before coming back here to inform your Campaign Manager of the choice.</p>\n";
 					
@@ -117,32 +117,50 @@
 					echo '<br>Germany.......... : Button : Button :<br>';
 					echo '<br>Austro-Hungary... : Button : Button :<br>';
 					*/
-					echo "<p>Next, back to the Mission editor. Make sure you are in 2D editing mode by clicking on the Arrow down ikon key at the left hand side of 
+					echo "<h3>Define the Influence Areas</h3>\n";
+					echo "<p>Next, go into 2D editing mode by clicking on the Arrow down icon key at the left hand side of 
 					the tool bar which you normally find at the top of the screen. It is in-between the ruler icon and the F icon.<br>\n";
 					echo "You can zoom in and out using the scroll on your mouse and drag the map around holding the right mouse click.<br>\n";
 					echo "The campaign maps (as opposed to the dogfight maps) are very big. If you populate the complete map with objects 
-					you will probably run into performance problems in a large scale multi user mission. So we will define a sector 
+					you will probably run into performance problems in a large scale multi user mission.  To avoid such problems we will define a smaller sector 
 					of the map within which to run our campaign. Note when you zoom in and zoom out in the editor down near the bottom right there is a value \"Grid(M)\".
-					This is the size in metres of the white grid squares which will give you an idea of the scale of your map on screen.<br>\n";
-echo "<p><b><font color=\"red\">NEED INSTRUCTIONS ON CONFIGURING INFLUENCE AREAS</font></b></p>\n";
+					This is the size in metres of the white grid squares which will give you an idea of the scale of your map on screen.</p\n";
+
 ?>
-<p>Instructions on configuring influence areas from SC/JG_Oesau in the Rise of Flight forums:<br />
-- Select the Influence Area<br />
-- Right click on Influence Area and select "Selected Object Menu"<br />
-- Select "Edit Influence Area"<br />
-- you will note a yellow circle at one end of the triangle, you can then move the end of the influence area<br />
-- To select other ends to edit, double click on each end<br />
-- To end the edit, right click on Influence Area and select "Selected Object Menu"<br />
-- Select "Stop Editing Boundary"</p>
-<p>And SYN_Vander adds:<br />
-And with CTRL+mouse click you can add more points...</p>
+<p>We will now define the influence areas for our campaign.</p>
+<p>The simplest case is two influence areas... one for each opposing side, and that is what we will go through here, but we also support multiple influence areas should you wish to use them. The combat area includes all defined influence areas.  Areas that are outside of defined influence areas are deemed neutral.</p>
 
+<p>To define the first influence area:<br \>
+* select the ">>MCUs<<" box in the upper right of the mission editor.<br />
+* Scroll down the list and select "Translator:Influence Area".<br \>
+* Left click to place this on the map within the area you have choosen to be, say, German territory.<br />
+* Select the influence area icon with another left click.<br />
+* Right click on the influence area icon and select "Selected Object Menu".<br />
+* Select "Edit Influence Area".<br />
+* This will create a blue triangle with a yellow circle at one apex which you can drag with your mouse (holding down the left mouse button) to move the end of the influence area.<br />
+* To select another end to edit, double click on that end.<br />
+* You will probably want at least one more corner to form a trapezoid.<br />
+* To add another point to move, use CTRL+left mouse click on a boundary line.<br />
+* Note that the boundary of an influence area should not cross itself.<br />
+* Move the ends to form a boundary around the area you want to define.<br />
+* To stop editing, right click on the influence area and select "Selected Object Menu".<br />
+* Select "Stop Editing Boundary".</p>
 
+<p>To set ownership of the influence area:<br />
+* Right click on the influence area and select "Advanced Properties..."<br /> 
+* select the country you want to be the owner of this area (e.g. Germany)<br />
+* click "OK".
+* Left click outside the influence area to unselect it.</p>
+
+<p>Repeat this process to define the opposing side's influence area(s).</p>
 <?php
+/*
 					echo "To define the sector of the map we want to use we will need the bottom left and top right of a rectangle. <br>\n";
 					echo "There are three dimensions to the map X is from the bottom to the top, Y is Height and Z is left to right.<br>\n"; 
 					echo "Down near the bottom right of the editor you also have the X and Z values of the mouse position. So choose where you want the bottom left
 					of the sector and note the X and Z values then the same for the top right of the sector.<br>\n";
+*/
+
 //					echo "Return to the Campaign manager and enter the values here. </p>\n";
 //					
 //					include ('includes/getMinMaxXZ.php');
@@ -162,9 +180,10 @@ And with CTRL+mouse click you can add more points...</p>
 //					echo "		<button type=\"submit\" name =\"updateCampaignParameters\" id=\"loginSubmit\" value =\"1\" >Apply Configuration</button>\n";	# the value defines the action after the button was pressed
 //					echo "	</fieldset>\n";
 					
+					echo "<h3>Refine the Template</h3>\n";
 					echo "<p>Next we will start to refine our campaign template<br>";
 					# the input from here will update the campaign fields in cam_param or campaign_statistics whatever
-					echo "<p>Return to the Mission editor. We now want to save just the objects that are in our sector. On the top icon bar is a button for \"OBJ FILT\" select that and make sure to \"Select All\" then OK.
+					echo "<p>We now want to save <b>just</b> the objects that are in our sector. On the top icon bar is a button for \"OBJ FILT\" select that and make sure to \"Select All\" then OK.
 					Using left mouse button drag from bottom left of our sector to top right of our sector. This will highlight all objects in our sector.
 					Next File, Save selection to File, select /data/Multiplayer/Dogfight/ and save the file as campaign_name-no-trunc.
 					A single left mouse click unselects. Now go to the Search and Select menu, Select All Objects in Mission and press the delete key on your keyboard. There will be a pause, patience and all the airfields etc. will dissapear.
