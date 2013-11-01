3ea3e874c58f54cc9173d2e7c178ac00606db976
69giapstenka <ptthome@free.fr>
2013-11-01 13:52:30 +0100

    creates a complete fresh test database
    You have to adjust your path with search replace

---
+++ Global SQL/new_database.sql
@@ -0,0 +1,45 @@
+source c:/Abyss web server/htdocs/global sql/boswar_db.sql;
+source c:/Abyss web server/htdocs/global sql/campaign_missions.sql;
+source c:/Abyss web server/htdocs/global sql/campaign_settings.sql;
+source c:/Abyss web server/htdocs/global sql/campaign_status.sql;
+source c:/Abyss web server/htdocs/global sql/campaign_users.sql;
+source c:/Abyss web server/htdocs/global sql/create_boswar_hq_user.sql;
+source c:/Abyss web server/htdocs/global sql/create_rofwar_campaign_user.sql;
+source c:/Abyss web server/htdocs/global sql/inbox.sql;
+source c:/Abyss web server/htdocs/global sql/maps.sql;
+source c:/Abyss web server/htdocs/global sql/mission_status.sql;
+source c:/Abyss web server/htdocs/global sql/player_fate.sql;
+source c:/Abyss web server/htdocs/global sql/player_health.sql;
+source c:/Abyss web server/htdocs/global sql/rof_airfields.sql;
+source c:/Abyss web server/htdocs/global sql/rof_channel_locations.sql;
+source c:/Abyss web server/htdocs/global sql/rof_coalitions.sql;
+source c:/Abyss web server/htdocs/global sql/rof_countries.sql;
+source c:/Abyss web server/htdocs/global sql/rof_gunner_scores.sql;
+source c:/Abyss web server/htdocs/global sql/rof_kills.sql;
+source c:/Abyss web server/htdocs/global sql/rof_lake_locations.sql;
+source c:/Abyss web server/htdocs/global sql/rof_models.sql;
+source c:/Abyss web server/htdocs/global sql/rof_object_properties.sql;
+source c:/Abyss web server/htdocs/global sql/rof_object_roles.sql;
+source c:/Abyss web server/htdocs/global sql/rof_pilot_scores.sql;
+source c:/Abyss web server/htdocs/global sql/rof_verdun_locations.sql;
+source c:/Abyss web server/htdocs/global sql/rof_westernfront_locations.sql;
+source c:/Abyss web server/htdocs/global sql/statistics_test.sql;
+source c:/Abyss web server/htdocs/global sql/supply_points.sql;
+source c:/Abyss web server/htdocs/global sql/tabcreate_Blocks.sql;
+source c:/Abyss web server/htdocs/global sql/tabcreate_bridges.sql;
+source c:/Abyss web server/htdocs/global sql/tabcreate_cam_param.sql;
+source c:/Abyss web server/htdocs/global sql/tabcreate_col_10.sql;
+source c:/Abyss web server/htdocs/global sql/tabcreate_Flags.sql;
+source c:/Abyss web server/htdocs/global sql/tabcreate_static.sql;
+source c:/Abyss web server/htdocs/global sql/tabcreate_Trains.sql;
+source c:/Abyss web server/htdocs/global sql/tabcreate_Vehicles.sql;
+source c:/Abyss web server/htdocs/global sql/users.sql;
+source c:/Abyss web server/htdocs/global sql/users_roles.sql;
+
+
+
+
+
+
+
+
