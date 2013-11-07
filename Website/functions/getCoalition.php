f6a1ed2071cbec045f98181ad41276754f2229b1
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-11-07 18:11:18 -0500

    update getCoalition.php
    changing mysqli calls to object oriented version

--- Website/functions/getCoalition.php
+++ Website/functions/getCoalition.php
@@ -3,22 +3,22 @@
 // given country key #, get current Coalition ID
 // =69.GIAP=TUSHKA
 // Oct 22, 2013
-// revised Oct 24, 2013
-// version 1.1 
+// revised Nov 7, 2013
+// version 1.2 
 
 // define the function 
 function get_coalition($ckey) {
 global $camp_link; // link to campaign db
 
-   $query = "SELECT CoalID FROM rof_countries WHERE ckey = '$ckey'";
-   if($result = mysqli_query($camp_link, $query)) {
-      while ($obj = mysqli_fetch_object($result)) {
-	 return($obj->CoalID);
-      }
-   } else {
-      die('getCoalition query error [' . $camp_link->error . ']');
-   }
-   // free result set
-   mysqli_free_result($result);
+	$query = "SELECT CoalID FROM rof_countries WHERE ckey = '$ckey'";
+	if($result = $camp_link->query($query)) {
+		while ($obj = $result->fetch_object($result)) {
+			return($obj->CoalID);
+		}
+	} else {
+		die('getCoalition query error [' . $camp_link->error . ']');
+	}
+		// free result set
+	$result->free();
 }
 ?>
