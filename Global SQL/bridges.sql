02a792240a1fa7b1a33e000623351ebc0b15a97c
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-11-21 12:06:56 -0500

    changing default bridge damage to NULL
    Myata, please let me know if this fixes the bridge import errors.
    I don't see these errors on my system, but we have seen other examples
    of our systems handling zero and NULL values in different ways.  I will
    note that when my system imports those blank '' entries, it inserts '0'
    into the column rather than NULL.

--- Global SQL/bridges.sql
+++ Global SQL/bridges.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2013 at 11:03 PM
+-- Generation Time: Nov 21, 2013 at 06:00 PM
 -- Server version: 5.6.11
 -- PHP Version: 5.5.3
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `boswar_db`
+-- Database: `skies_of_the_empires`
 --
 
 -- --------------------------------------------------------
@@ -29,25 +29,25 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `bridges`;
 CREATE TABLE IF NOT EXISTS `bridges` (
   `id` int(1) NOT NULL AUTO_INCREMENT,
-  `Name` char(31) DEFAULT 'Bridge',
+  `Name` char(31) DEFAULT NULL,
   `Model` char(20) DEFAULT NULL,
   `Description` varchar(80) DEFAULT NULL,
-  `Country` enum('0','101','102','103','104','105','501','502','600','610','620','630','640') DEFAULT '0',
-  `CoalID` enum('0','1','2') DEFAULT '0',
-  `XPos` decimal(12,3) DEFAULT '0.000',
-  `ZPos` decimal(12,3) DEFAULT '0.000',
-  `YOri` decimal(5,2) DEFAULT '0.00',
-  `updated` tinyint(1) DEFAULT '0',
-  `damage_1` tinyint(1) DEFAULT '0',
-  `damage_2` tinyint(1) DEFAULT '0',
-  `damage_3` tinyint(1) DEFAULT '0',
-  `damage_4` tinyint(1) DEFAULT '0',
-  `damage_5` tinyint(1) DEFAULT '0',
-  `damage_6` tinyint(1) DEFAULT '0',
-  `damage_7` tinyint(1) DEFAULT '0',
-  `damage_8` tinyint(1) DEFAULT '0',
-  `damage_9` tinyint(1) DEFAULT '0',
-  `damage_10` tinyint(1) DEFAULT '0',
+  `Country` enum('0','101','102','103','104','105','501','502','600','610','620','630','640') DEFAULT NULL,
+  `CoalID` enum('0','1','2') DEFAULT NULL,
+  `XPos` decimal(12,3) DEFAULT NULL,
+  `ZPos` decimal(12,3) DEFAULT NULL,
+  `YOri` decimal(5,2) DEFAULT NULL,
+  `updated` tinyint(1) DEFAULT NULL,
+  `damage_1` tinyint(1) DEFAULT NULL,
+  `damage_2` tinyint(1) DEFAULT NULL,
+  `damage_3` tinyint(1) DEFAULT NULL,
+  `damage_4` tinyint(1) DEFAULT NULL,
+  `damage_5` tinyint(1) DEFAULT NULL,
+  `damage_6` tinyint(1) DEFAULT NULL,
+  `damage_7` tinyint(1) DEFAULT NULL,
+  `damage_8` tinyint(1) DEFAULT NULL,
+  `damage_9` tinyint(1) DEFAULT NULL,
+  `damage_10` tinyint(1) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
