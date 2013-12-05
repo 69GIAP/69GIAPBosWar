1c82b42259faf2887ef1a81815dc2ab623b018b2
=69.GIAP=MYATA <rowbajt@gmail.com>
2013-12-05 20:48:51 +0100

    minor changes - bug not yet fixed
    Mantis #0000003

--- Website/CampaignMgmtConfigureConfirm.php
+++ Website/CampaignMgmtConfigureConfirm.php
@@ -18,11 +18,20 @@
     
             <div id="content">
 				<?php
+					# include connect2CampaignFunction.php
+					include ( 'functions/connect2Campaign.php' );
+
+					# include getCampaignVariables.php
+					include ( 'includes/getCampaignVariables.php' );
+	
+					# use this information to connect to campaign 
+					$camp_link = connect2campaign("$camp_host","$camp_user","$camp_passwd","$loadedCampaign");				
+					
 					$newCampStatus = $_POST["newCampStatus"];
 						
 				if ($_POST["createCampaign"] == 1)
 					{
-					$query="UPDATE campaign_settings SET status = $newCampStatus WHERE camp_db = '$loadedCampaign'";
+					$query="UPDATE campaign_settings SET status = $newCampStatus WHERE camp_db like '$loadedCampaign'";
 					}
 				if ($_POST["createCampaign"] == 2)
 					{
@@ -33,9 +42,14 @@
 				# updates
 				if(!$result = $camp_link->query($query)){
 					die('There was an error running the query <br>'.$query."<br>" . $camp_link->error());
+					// close $camp_link
+					$camp_link->close();
 				}
 					else
-				{				
+				{
+					// close $camp_link
+					$camp_link->close();
+						
 					header ("Location: CampaignMgmtChangeStatus.php?btn=campStp");
 				}
                 ?>					
--- Website/includes/getCampaignVariables.php
+++ Website/includes/getCampaignVariables.php
@@ -1,31 +1,30 @@
 <?php
-					# get remaining variables to connect to campaign 
-					$query = "SELECT * from campaign_settings where camp_db = '$loadedCampaign'";  
-		 
-					if(!$result = $dbc->query($query)) {
-						die('There was an error running the query [' . $dbc->error . ']');
-					}
-		
-					if ($result = $dbc->query($query)) {
-						/* fetch associative array */
-						while ($obj = $result->fetch_object()) {
-							$campaign	=($obj->campaign);
-							$abbrv		=($obj->abbrv);
-							$camp_host	=($obj->camp_host);
-							$camp_user	=($obj->camp_user);
-							$camp_passwd=($obj->camp_passwd);
-							$camp_status_id=($obj->status);
-							
-							# get campaign status
-							$sql="SELECT campaign_status FROM campaign_status where id = $camp_status_id";
-							if ($result = $dbc->query($sql)) {
-							/* fetch associative array */
-							while ($obj = $result->fetch_object()) {
-								$camp_status=($obj->campaign_status);
-								}
-							}
-						}
-					} 
-					# free result set
-					$result->close();
+	$query = "SELECT * from campaign_settings where camp_db = '$loadedCampaign'";  
+	
+	if(!$result = $dbc->query($query)) {
+		die('There was an error running the query [' . $dbc->error . ']');
+	}
+	
+	if ($result = $dbc->query($query)) {
+		/* fetch associative array */
+		while ($obj = $result->fetch_object()) {
+			$campaign		=($obj->campaign);
+			$abbrv			=($obj->abbrv);
+			$camp_host		=($obj->camp_host);
+			$camp_user		=($obj->camp_user);
+			$camp_passwd	=($obj->camp_passwd);
+			$camp_status_id	=($obj->status);
+			
+			# get campaign status
+			$sql="SELECT campaign_status FROM campaign_status where id = $camp_status_id";
+			if ($result = $dbc->query($sql)) {
+			/* fetch associative array */
+			while ($obj = $result->fetch_object()) {
+				$camp_status=($obj->campaign_status);
+				}
+			}
+		}
+	} 
+	# free result set
+	$result->close();
 ?>
