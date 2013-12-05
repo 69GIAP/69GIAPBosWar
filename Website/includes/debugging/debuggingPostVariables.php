0879486973a2e6505081f5805d9a8e40648a58ea
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2013-12-04 23:49:55 -0500

    update debuggingPostVariables.php
    Added ability to display arrays.

--- Website/includes/debugging/debuggingPostVariables.php
+++ Website/includes/debugging/debuggingPostVariables.php
@@ -2,7 +2,13 @@
 	echo "\n <div class=\"debuggingPost\">\n";
 	# TUSHKA - dump all POST variables for debugging
 	foreach ($_POST as $param_name => $param_val) {
-		echo "\$_POST $param_name: <b>$param_val</b><br> \n";
+		if (is_array($param_val)) {
+				echo "\$_POST $param_name: <br />\n";
+				print_r(array_values($param_val));
+				echo "<br />\n";
+		} else {
+			echo "\$_POST $param_name: <b>$param_val</b><br />\n";
+		}
 	}
 	echo "</div> \n";
 ?>
