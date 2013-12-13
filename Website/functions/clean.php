8c02ff6e1a9f6d543e500be320aab2b6b666f3d8
69giapstenka <ptthome@free.fr>
2013-12-13 08:54:26 +0100

    clean function

---
+++ Website/functions/clean.php
@@ -0,0 +1,7 @@
+<?php
+# function to clean special characters from a string
+# Stenka 12/12/13
+function clean($string) {
+   $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+}
