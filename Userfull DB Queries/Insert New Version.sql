d8cc0ece36f45f55c953f566fd739f93868e1b97
=69.GIAP=MYATA <rowbajt@gmail.com>
2014-05-18 10:54:55 +0200

    pushed new version as DB structure changes were applied
    I kept the original entries in the table as the query filters for the
    latest entry anyway. This way we have a history. I also added a remark
    and revisor column which is only visible in the db so we know who and
    why the change was pushed.

--- Userfull DB Queries/Insert New Version.sql
+++ Userfull DB Queries/Insert New Version.sql
@@ -1,6 +1,9 @@
 
-INSERT into version (simulation, dbVersion, buildDate, description)
-SELECT 'RoF', '0.1.0.3','2013-11-30 14:30:00', 'Alpha Phase: Version ';
+INSERT into version (simulation, dbVersion, buildDate, description, remark, revisor)
+SELECT 'RoF', '0.1.0.10', NOW(), 'Alpha Phase: Version ', 'campaing configuration tracking introduced that required tables; GitHub: be70091878532f365ce0dac64b9a78cbacd59522 ', 'MYATA';
+
+INSERT into version (simulation, dbVersion, buildDate, description, remark, revisor)
+SELECT 'BoS', '0.0.0.10', NOW(), 'Alpha Phase: Version ', 'sidebar was synched with RoF sidebar', 'MYATA';
 
 SELECT * FROM boswar_db.version
 order by simulation, buildDate ;
