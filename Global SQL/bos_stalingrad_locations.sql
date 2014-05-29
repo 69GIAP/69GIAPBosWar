4ea8e6a769d5d29ce3c61b75c0070ef037aab9f5
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2014-05-29 16:12:31 -0400

    update bos_stalingrad_locations.sql
    Adding the airfields and nearby towns/villages assumed to be of the same
    name from the NW sector of the Stalingrad map.  The airfields map is now
    approximately half done.

--- Global SQL/bos_stalingrad_locations.sql
+++ Global SQL/bos_stalingrad_locations.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 28, 2014 at 03:15 PM
+-- Generation Time: May 29, 2014 at 04:08 PM
 -- Server version: 5.6.11
 -- PHP Version: 5.5.3
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `bos_stalingrad_locations` (
   `LName` varchar(40) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `LName` (`LName`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=63 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=102 ;
 
 --
 -- Dumping data for table `bos_stalingrad_locations`
@@ -103,7 +103,46 @@ INSERT INTO `bos_stalingrad_locations` (`id`, `LID`, `LX`, `LZ`, `LName`) VALUES
 (59, 51, '23280', '235000', 'Umantchevo'),
 (60, 10, '83750', '238560', 'Tinguta'),
 (61, 51, '84310', '235910', 'Tinguta'),
-(62, 10, '127330', '2287870', 'Basargino');
+(62, 10, '127330', '2287870', 'Basargino'),
+(63, 10, '160360', '115960', 'Karaichev'),
+(64, 51, '160650', '114320', 'Karaichev'),
+(65, 10, '163040', '136480', 'Manoylin'),
+(66, 51, '166940', '136350', 'Manoylin'),
+(67, 10, '197970', '194340', 'Kletskaya'),
+(68, 51, '199910', '148740', 'Kletskaya'),
+(69, 10, '144860', '158870', 'Skvorin'),
+(70, 51, '145240', '160460', 'Skvorin'),
+(71, 10, '143590', '163580', 'Berezovskiy'),
+(72, 51, '143840', '162590', 'Berezovskiy'),
+(73, 10, '186050', '165540', 'Ventsy'),
+(74, 51, '185280', '167470', 'Ventsy'),
+(75, 10, '148020', '180530', 'Golubinskiy'),
+(76, 51, '147810', '184640', 'Golubinskiy'),
+(77, 10, '153790', '184480', 'Malogolubaya'),
+(78, 51, '153710', '186740', 'Malogolubaya'),
+(79, 10, '163100', '188250', 'Evlampiyevskiy'),
+(80, 51, '162620', '189850', 'Evlampiyevskiy'),
+(81, 20, '187220', '192000', 'Zimovskiy'),
+(82, 20, '187370', '192550', 'Zimovskiy'),
+(83, 51, '188250', '194290', 'Zimovskiy'),
+(84, 10, '152220', '204500', 'Peskatovka'),
+(85, 51, '153990', '202070', 'Peskatovka'),
+(86, 20, '135180', '214720', 'Karpovka'),
+(87, 20, '134280', '214370', 'Karpovka'),
+(88, 51, '131420', '214630', 'Karpovka'),
+(89, 10, '172020', '218870', 'Kachalinskaya'),
+(90, 51, '175820', '218620', 'Kachalinskaya'),
+(91, 10, '194600', '226110', 'Illarionovskiy'),
+(92, 10, '142290', '228430', 'Bolshaya'),
+(93, 51, '144930', '228090', 'Bolshaya'),
+(94, 20, '136170', '233890', 'Pitomnik'),
+(95, 20, '135790', '233910', 'Pitomnik'),
+(96, 51, '134650', '231630', 'Pitomnik'),
+(97, 10, '159280', '234520', 'Kotluban'),
+(98, 51, '158220', '232640', 'Kotluban'),
+(99, 20, '214850', '238760', 'Bolshaya Ivanovka'),
+(100, 20, '214340', '239500', 'Bolshaya Ivanovka'),
+(101, 51, '214680', '241390', 'Bolshaya Ivanovka');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
