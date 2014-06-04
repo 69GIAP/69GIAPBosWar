a5f198652472538a7c5e54d5a916174c52ce4a70
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2014-06-03 21:44:01 -0400

    update version.sql
    Because of major changes to BoS db files, and to keep Myata happy.  :)

--- Global SQL/version.sql
+++ Global SQL/version.sql
@@ -1,30 +1,33 @@
-CREATE DATABASE  IF NOT EXISTS `boswar_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `boswar_db`;
--- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
 --
--- Host: 10.0.0.27    Database: boswar_db
--- ------------------------------------------------------
--- Server version	5.6.15
+-- Host: 127.0.0.1
+-- Generation Time: Jun 03, 2014 at 09:42 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `boswar_db`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `version`
 --
 
 DROP TABLE IF EXISTS `version`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `version` (
+CREATE TABLE IF NOT EXISTS `version` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `simulation` varchar(3) NOT NULL,
   `dbVersion` varchar(10) NOT NULL,
@@ -33,26 +36,19 @@ CREATE TABLE `version` (
   `remark` text,
   `revisor` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`,`simulation`,`dbVersion`,`buildDate`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `version`
 --
 
-LOCK TABLES `version` WRITE;
-/*!40000 ALTER TABLE `version` DISABLE KEYS */;
-INSERT INTO `version` VALUES (2,'BoS','0.0.0.9','2013-11-21 11:30:45','Pre Alpha Phase: Version ',NULL,NULL),(3,'RoF','0.1.0.9','2013-12-05 22:37:00','Alpha Phase: Version ',NULL,NULL),(6,'RoF','0.1.0.10','2014-05-18 10:52:46','Alpha Phase: Version ','campaing configuration tracking introduced that required tables; GitHub: be70091878532f365ce0dac64b9a78cbacd59522 ','MYATA'),(7,'BoS','0.0.0.10','2014-05-18 10:52:46','Alpha Phase: Version ','sidebar was synched with RoF sidebar','MYATA');
-/*!40000 ALTER TABLE `version` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `version` (`id`, `simulation`, `dbVersion`, `buildDate`, `description`, `remark`, `revisor`) VALUES
+(2, 'BoS', '0.0.0.9', '2013-11-21 11:30:45', 'Pre Alpha Phase: Version ', NULL, NULL),
+(3, 'RoF', '0.1.0.9', '2013-12-05 22:37:00', 'Alpha Phase: Version ', NULL, NULL),
+(6, 'RoF', '0.1.0.10', '2014-05-18 10:52:46', 'Alpha Phase: Version ', 'campaing configuration tracking introduced that required tables; GitHub: be70091878532f365ce0dac64b9a78cbacd59522 ', 'MYATA'),
+(7, 'BoS', '0.0.0.10', '2014-05-18 10:52:46', 'Alpha Phase: Version ', 'sidebar was synched with RoF sidebar', 'MYATA'),
+(8, 'BoS', '0.1.0.12', '2014-06-03 23:38:10', 'Alpha Phase: Version ', 'Completed preliminary bos_stalingrad_locations and bos_object_properties.', 'TUSHKA');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-05-18 10:53:33
