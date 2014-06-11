6cc0d9d1b462ed6ae33ec94e5745b12b9ec99d14
=69.GIAP=TUSHKA <69giaptushka@gmail.com>
2014-06-11 17:32:50 -0400

    update airfields.sql
    Correcting a missing single quote after '201' that made the previous
    version invalid for import.

--- Global SQL/airfields.sql
+++ Global SQL/airfields.sql
@@ -1,34 +1,38 @@
-
--- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
 --
--- Host: 10.0.0.57    Database: chanclfct
--- ------------------------------------------------------
--- Server version	5.6.13
+-- Host: 127.0.0.1
+-- Generation Time: Jun 11, 2014 at 05:30 PM
+-- Server version: 5.6.11
+-- PHP Version: 5.5.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `boswar_db`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `airfields`
 --
-#Stenka 11/6/14 adding country 201 for BoS
+
 DROP TABLE IF EXISTS `airfields`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `airfields` (
+CREATE TABLE IF NOT EXISTS `airfields` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `airfield_Name` char(31) DEFAULT 'Unknown airfield',
   `airfield_Model` char(20) DEFAULT NULL,
   `airfield_Desc` varchar(80) DEFAULT NULL,
-  `airfield_Country` enum('0','101','102','103','104','105','201,'501','502','600','610','620','630','640') DEFAULT '0',
+  `airfield_Country` enum('0','101','102','103','104','105','201','501','502','600','610','620','630','640') DEFAULT '0',
   `airfield_Coalition` enum('0','1','2','3','4','5','6','7') DEFAULT '0',
   `airfield_XPos` decimal(12,3) DEFAULT '0.000',
   `airfield_ZPos` decimal(12,3) DEFAULT '0.000',
@@ -37,16 +41,8 @@ CREATE TABLE `airfields` (
   `airfield_Enabled` int(11) DEFAULT '0',
   `airfield_Updated` int(11) DEFAULT '0',
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=63 ;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-11-07  7:56:43
