d7de45acb3589a8f64dbc711f73cd775a61e5ade
=69.GIAP=MYATA <rowbajt@gmail.com>
2014-08-06 10:47:24 +0200

    POST variabel feedback turned off

--- Website/includes/header.php
+++ Website/includes/header.php
@@ -89,7 +89,7 @@ session_start();
 		}
 	##### DEBUGGING #####
 #	include ('includes/debugging/debuggingSessionVariables.php');	
- 	include ('includes/debugging/debuggingPostVariables.php' ); #testing the Post variables for my objects enabling this creates an error so I turned it off temporarily.
+# 	include ('includes/debugging/debuggingPostVariables.php' ); #testing the Post variables for my objects enabling this creates an error so I turned it off temporarily.
 #	include ('includes/debugging/debuggingMySqlError.php' );	
 			
 ?>
